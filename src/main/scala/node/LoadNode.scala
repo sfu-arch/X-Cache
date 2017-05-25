5d8b5fc4f2f4ec0f23e5662edb3e30ad4d9387ce
vnaveen0 <vnaveen0@gmail.com>
2017-05-25 16:21:02 -0700

    Converted Loads to Vectors need to test throughly

--- src/main/scala/node/LoadNode.scala
+++ src/main/scala/node/LoadNode.scala
@@ -29,30 +29,30 @@ abstract class LoadIO(val NumMemOP :Int = 1, val ID :Int = 0)
     val memReq  = Decoupled(new ReadReq())
     val memResp = Input(Flipped(new ReadResp()))
 
-
     val memOpAck  = new RvAckIO()
-    //    val memOpAck = Decoupled(UInt(1.W)) //TODO 0 bits
 
   })
 }
 
 
-class LoadNode(implicit p: Parameters) extends LoadIO()(p){
+class   LoadNode(implicit p: Parameters) extends LoadIO()(p){
 
   //---------------------------------------------------------------------------------
   val nodeID_reg = RegInit(ID.U)
   val addr_reg = RegInit(0.U(xlen.W))
   val addr_valid_reg = RegInit(false.B)
 
-  // Status Register - If src mem-ops done execution
-  val in3_done_reg = RegInit(false.B)
-
   // Initialization registers to send ready signals
   // to all inputs to accept input nodes
   val init1_reg = RegInit(false.B)
 
   //TODO Make init3_reg a Vector of registers activated based on input predMemOp
-  val init3_reg = RegInit(false.B)
+  //  val init3_reg = RegInit(false.B)
+  val init3_reg = RegInit(Vec(Seq.fill(NumMemOP)(false.B)))
+
+  // Status Register - If src mem-ops done execution
+  //  val in3_done_reg = RegInit(false.B)
+  val in3_done_reg = RegInit(Vec(Seq.fill(NumMemOP)(false.B)))
 
 
   //Mem ready for response
@@ -63,14 +63,13 @@ class LoadNode(implicit p: Parameters) extends LoadIO()(p){
 
   //Initialization
   io.gepAddr.ready      := ~init1_reg
-  io.predMemOp(0).ready := ~init3_reg
-
-  //  printf( p"\n LdNODE: gepAddrValid ${io.gepAddr.valid} \n")
-  //  printf( p"\n LdNODE: gepAddrReady ${io.gepAddr.ready} \n")
+//  io.predMemOp(0).ready := ~init3_reg
+  for( w <- 0 until NumMemOP) {
+    io.predMemOp(w).ready := ~init3_reg(w)
+  }
 
   //---------------------------------------------------------------------------------
 
-  //-----------------------------------
 
   printf(p"Ld Node: GepAddr.valid: ${io.gepAddr.valid} " +
     p" GepAddr.ready: ${io.gepAddr.ready} GepAddr.bits: ${io.gepAddr.bits} \n")
@@ -86,11 +85,13 @@ class LoadNode(implicit p: Parameters) extends LoadIO()(p){
 
 
   //Rules for predMemOp(0)
-  when(io.predMemOp(0).fire()) {
+  for( i <- 0 until NumMemOP) {
+    when(io.predMemOp(i).fire()) {
 
-    //    printf(p"\n ---------------- predMemOp(0). fire -------------------\n")
-    in3_done_reg := true.B
-    init3_reg := true.B
+      //    printf(p"\n ---------------- predMemOp(0). fire -------------------\n")
+      in3_done_reg(i) := true.B
+      init3_reg(i) := true.B
+    }
   }
 
   //-----------------------------------
@@ -98,8 +99,12 @@ class LoadNode(implicit p: Parameters) extends LoadIO()(p){
   //Rules for Sending address and data to Memory
   // Note Memreq_addr.ready and Memreq_data.ready are connected
   // Store Node cannot send the data to memory unless all its predecessors are done: in3 in this case
+  // XOR 00 0, 10 1,
+  val test_reg = RegInit(0.U(NumMemOP.W))
+  val predMemOp_bool = init3_reg.asUInt() ^ test_reg
+  val predMemOpDone_bool = in3_done_reg.asUInt() ^ test_reg
 
-  val mem_req_fire = addr_valid_reg & in3_done_reg & init1_reg & init3_reg
+  val mem_req_fire = addr_valid_reg &  init1_reg & predMemOp_bool & predMemOpDone_bool
   io.memReq.valid := mem_req_fire
   io.memReq.bits.address := addr_reg
   io.memReq.bits.node := nodeID_reg
@@ -110,7 +115,10 @@ class LoadNode(implicit p: Parameters) extends LoadIO()(p){
 
     memresp_ready_reg := true.B
     addr_valid_reg := false.B
-    in3_done_reg := false.B
+
+    for(i <- 0 until NumMemOP) {
+      in3_done_reg(0) := false.B
+    }
     //    printf(p"\n ------------------ Mem Request Sent ------------------------- \n")
   }
 
@@ -145,7 +153,9 @@ class LoadNode(implicit p: Parameters) extends LoadIO()(p){
 
     data_resp_valid := false.B
     init1_reg := false.B
-    init3_reg := false.B
+    for ( w <- 0 until NumMemOP) {
+      init3_reg(w) := false.B
+    }
 
     //    printf(p"\n ------------------- Iteration Over ---------------------\n")
   }
