420d62f4a456b496f57224c010ae3f2abaac873c
amiralis <amirali_sharifian@sfu.ca>
2017-06-10 12:46:14 -0700

    Add file

---
+++ src/test/scala/node/computeNode.scala
@@ -0,0 +1,82 @@
+// See LICENSE for license details.
+
+package node
+
+import chisel3._
+import chisel3.util._
+
+import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester, OrderedDecoupledHWIOTester}
+import org.scalatest.{Matchers, FlatSpec}
+
+import node._
+import dataflow._
+import muxes._
+import config._
+import util._
+import interfaces._
+
+
+
+
+
+// Tester.
+class computeTester(df: ComputeNode)
+                  (implicit p: config.Parameters) extends PeekPokeTester(df)  {
+
+  poke(df.io.LeftIO.bits.data, 9.U)
+  poke(df.io.LeftIO.valid, true.B)
+
+  poke(df.io.RightIO.bits.data, 5.U)
+  poke(df.io.RightIO.valid, true.B)
+
+  step(1)
+
+  poke(df.io.LeftIO.bits.predicate, true.B)
+  poke(df.io.RightIO.bits.predicate, true.B)
+  for( i <- 0 until 10){
+    step(1)
+  }
+//  poke(df.io.CmpIn.valid, false.B)
+//  poke(df.io.CmpIn.bits, false.B)
+//
+//  println(s"Node input : ${peek(df.io.CmpIn)}")
+//  println(s"Node output: ${peek(df.io.OutIO)}")
+//
+//  step(1)
+//
+//  poke(df.io.CmpIn.bits, true.B)
+//  println(s"Node input : ${peek(df.io.CmpIn)}")
+//  println(s"Node output: ${peek(df.io.OutIO)}")
+//
+//  poke(df.io.CmpIn.valid, true.B)
+//  step(1)
+//  println(s"Node input : ${peek(df.io.CmpIn)}")
+//  println(s"Node output: ${peek(df.io.OutIO)}")
+//
+//  step(1)
+//  println(s"Node input : ${peek(df.io.CmpIn)}")
+//  println(s"Node output: ${peek(df.io.OutIO)}")
+//  step(1)
+//
+//  poke(df.io.CmpIn.bits, false.B)
+//  println(s"Node input : ${peek(df.io.CmpIn)}")
+//  println(s"Node output: ${peek(df.io.OutIO)}")
+//  step(1)
+//  println(s"Node input : ${peek(df.io.CmpIn)}")
+//  println(s"Node output: ${peek(df.io.OutIO)}")
+ }
+
+
+
+
+class CompTests extends  FlatSpec with Matchers {
+   implicit val p = config.Parameters.root((new MiniConfig).toInstance)
+  it should "Dataflow tester" in {
+     chisel3.iotesters.Driver(() => new ComputeNode(NumOuts = 1, ID = 0, opCode = 1)) {
+       c => new computeTester(c)
+     } should be(true)
+   }
+ }
+
+
+
