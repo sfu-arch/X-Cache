065e9ebc7d183bd13e362596462a0be86dbccd9a
amiralis <amirali_sharifian@sfu.ca>
2017-06-22 15:10:23 -0700

    Add parameter to dataflow example

--- src/main/scala/dataflow/AdditionDF.scala
+++ src/main/scala/dataflow/AdditionDF.scala
@@ -15,6 +15,55 @@ import regfile._
 import node._
 
 
+/**
+  * This Object should be initialize at the first step
+  */
+object functionParam{
+
+  val b0_entry_pred = Map(
+    "active" -> 0
+  )
+
+  val b1_then_pred = Map(
+    "m1" -> 0
+  )
+
+  val b2_end_pred = Map(
+    "m1" -> 0,
+    "m3" -> 1
+  )
+
+  val m1_brn_bb = Map(
+    "b1_then" -> 0,
+    "b2_end"  -> 1
+  )
+
+  val m3_ubrn_bb = Map(
+    "b2_end" -> 0
+  )
+
+  val b0_entry_activate = Map(
+    "m0" -> 0,
+    "m1" -> 1
+  )
+
+  val b1_then_activate = Map(
+    "m2" -> 0,
+    "m3" -> 1
+  )
+
+  val b2_end_activate = Map(
+    "m4" -> 0,
+    "m5" -> 1
+  )
+
+  val m4_phi_in = Map(
+    "const1" -> 0,
+    "m3" -> 1
+  )
+
+}
+
 //TODO uncomment if you remove StackCentral.scala file
 //
 abstract class AddDFIO(implicit val p: Parameters) extends Module with CoreParams {
@@ -28,6 +77,9 @@ abstract class AddDFIO(implicit val p: Parameters) extends Module with CoreParam
 class AddDF(implicit p: Parameters) extends AddDFIO()(p) {
 
 
+  /**
+    * @note Module's variables they should set during initialization
+    */
   //BasicBlock
   val b0_entry = Module(new BasicBlockNoMaskNode(NumInputs = 1, NumOuts = 2, BID = 0)(p))
   val b1_then = Module(new BasicBlockNoMaskNode(NumInputs = 1, NumOuts = 2, BID = 1)(p))
@@ -40,9 +92,13 @@ class AddDF(implicit p: Parameters) extends AddDFIO()(p) {
   val m2 = Module(new ComputeNode(NumOuts = 1, ID = 2, opCode = "Add")(p))
   val m3 = Module(new UBranchNode(ID = 3)(p))
 
-  val m4 = Module(new PhiNode(NumInputs = 2, NumOuts = 1, ID = 4))
+  val m4 = Module(new PhiNode(NumInputs = 2, NumOuts = 1, ID = 4)(p))
   val m5 = Module(new ComputeNode(NumOuts = 1, ID = 5, opCode = "Add")(p))
 
+  /**
+    * Instantiating parameters
+    */
+  val param = functionParam
 
   /**
     * Wireing control signals from BasicBlock nodes
@@ -50,42 +106,41 @@ class AddDF(implicit p: Parameters) extends AddDFIO()(p) {
     */
 
   //Grounding entry BasicBlock
-  b0_entry.io.predicateIn(0).bits:= ControlBundle.Activate
-  b0_entry.io.predicateIn(0).valid := true.B
+  b0_entry.io.predicateIn(param.b0_entry_pred("active")).bits := ControlBundle.Activate
+  b0_entry.io.predicateIn(param.b0_entry_pred("active")).valid := true.B
 
   /**
     * Connecting basic blocks to predicate instructions
     */
   //Connecting m1 to b1_then
-  b1_then.io.predicateIn(0) <> m1.io.Out(0)
+  b1_then.io.predicateIn(param.b1_then_pred("m1")) <> m1.io.Out(param.m1_brn_bb("b1_then"))
 
   //Connecting m1 to b2_end
-  b2_end.io.predicateIn(0) <> m1.io.Out(1)
+  b2_end.io.predicateIn(param.b2_end_pred("m1")) <> m1.io.Out(param.m1_brn_bb("b2_end"))
 
   //Connecting m3 to b2_end
-  b2_end.io.predicateIn(1) <> m3.io.Out(0)
-
+  b2_end.io.predicateIn(param.b2_end_pred("m3")) <> m3.io.Out(param.m3_ubrn_bb("b2_end"))
 
 
   /**
     * Wireing enable signals to the instructions
     */
   //Wiring enable signals
-  m0.io.enable <> b0_entry.io.Out(0)
-  m1.io.enable <> b0_entry.io.Out(1)
+  m0.io.enable <> b0_entry.io.Out(param.b0_entry_activate("m0"))
+  m1.io.enable <> b0_entry.io.Out(param.b0_entry_activate("m1"))
 
-  m2.io.enable <> b1_then.io.Out(0)
-  m3.io.enable <> b1_then.io.Out(1)
+  m2.io.enable <> b1_then.io.Out(param.b1_then_activate("m2"))
+  m3.io.enable <> b1_then.io.Out(param.b1_then_activate("m3"))
 
-  m4.io.enable <> b2_end.io.Out(0)
-  m5.io.enable <> b2_end.io.Out(1)
+  m4.io.enable <> b2_end.io.Out(param.b2_end_activate("m4"))
+  m5.io.enable <> b2_end.io.Out(param.b2_end_activate("m5"))
 
   /**
     * Connecting PHI nodes
     */
   //Connect PHI node
-  m4.io.InData(0) <> m2.io.Out(0)
-  m4.io.InData(1) <> m2.io.Out(0)
+  m4.io.InData(param.m4_phi_in("m3")) <> m2.io.Out(0)
+  m4.io.InData(param.m4_phi_in("const1")) <> io.Data0
   m4.io.Mask <> b2_end.io.MaskBB(0)
 
 
@@ -103,7 +158,7 @@ class AddDF(implicit p: Parameters) extends AddDFIO()(p) {
 
   /**
     * Wireing constants
-   */
+    */
   m0.io.RightIO.bits.data := 9.U
   m0.io.RightIO.bits.predicate := true.B
   m0.io.RightIO.bits.valid := true.B
