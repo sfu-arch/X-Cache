d75adab4e85a3368aaec6949be747179ac15edb4
vnaveen0 <vnaveen0@gmail.com>
2017-07-09 17:57:10 -0700

    Adding test case for ReadTypTableEntry

---
+++ src/test/scala/memory/ReadTypTableEntry.scala
@@ -0,0 +1,116 @@
+package memory
+
+/**
+  * Created by vnaveen0 on 8/7/17.
+  */
+
+import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester, OrderedDecoupledHWIOTester}
+import org.scalatest.{Matchers, FlatSpec}
+
+import config._
+
+class ReadTypTableEntryTests(c: ReadTypTableEntry)
+                            (implicit p: config.Parameters)
+  extends PeekPokeTester(c) {
+
+  // -- IO ---
+  //      val NodeReq = Flipped(Decoupled(Input(new ReadReq)))
+  //    val NodeResp = Decoupled(new ReadResp)
+  //
+  //    //Memory interface
+  //    val MemReq = Decoupled(new CacheReq)
+  //    val MemResp = Input(new CacheResp)
+  //
+  //    // val Output
+  //    val output = Decoupled(new ReadResp)
+  //
+  //    val free = Output(Bool())
+  //    val done = Output(Bool())
+  // -- --------------------------------------------------
+
+  for (t <- 0 until 10) {
+
+    //    var tag : chisel3.core.UInt
+    //    var tag : Int = 12
+
+
+
+
+    //Send a Valid Node Request in Clock 3
+    if(t==3) {
+      poke(c.io.NodeReq.valid,1)
+      poke(c.io.NodeReq.bits.address,123)
+      poke(c.io.NodeReq.bits.Typ,4)
+      poke(c.io.NodeReq.bits.node,2)
+    }
+    else {
+      poke(c.io.NodeReq.valid, 0)
+    }
+
+    println(s"t : ${t} ----------------------------")
+    println(s"NodeReq.ready: ${peek(c.io.NodeReq.ready)}")
+    println(s"NodeReq.valid: ${peek(c.io.NodeReq.valid)}")
+    println(s"free : ${peek(c.io.free)}")
+    println(s"done : ${peek(c.io.done)}")
+
+    if(t>2) {
+
+      //Cache Req
+      //      val addr    = UInt(xlen.W)
+      //  val data    = UInt(xlen.W)
+      //  val mask    = UInt((xlen/8).W)
+      //  val tag     = UInt((List(1,rdmshrlen,wrmshrlen).max).W)
+      //  val iswrite = Bool()
+      println(s"MemReq.Valid: ${peek(c.io.MemReq.valid)}")
+      println(s"MemReq.addr: ${peek(c.io.MemReq.bits.addr)}")
+      println(s"MemReq.data: ${peek(c.io.MemReq.bits.data)}")
+      println(s"MemReq.mask: ${peek(c.io.MemReq.bits.mask)}")
+      println(s"MemReq.tag: ${peek(c.io.MemReq.bits.tag)}")
+      println(s"MemReq.isWrite: ${peek(c.io.MemReq.bits.iswrite)}")
+
+
+
+      println(s"MemResp.data: ${peek(c.io.MemResp.data)}")
+      println(s"MemResp.tag: ${peek(c.io.MemResp.tag)}")
+    }
+
+
+    //    var tag  = c.io.MemReq.bits.tag
+    // memory gets Ready at Clock 5 -> For First Address
+    if(t==4) {
+      poke(c.io.MemReq.ready, 1)
+      println(s"Sending Valid Request to Memory")
+      //      tag = c.io.MemReq.bits.tag
+    }
+    // Memory Responds with the data
+    if(t==6) {
+
+      println(s"Sending Valid Response from Memory")
+      poke(c.io.MemResp.data, 34)
+      poke(c.io.MemResp.tag, 0)
+      poke(c.io.MemResp.valid, 1)
+    }
+
+    // memory again gets Ready at Clock 7 -> For First Address
+    if(t > 6) {
+      poke(c.io.MemReq.ready, 1)
+      poke(c.io.MemReq.ready, 1)
+    }
+
+    step(1)
+
+  }
+
+}
+
+
+
+
+class ReadTypTableEntryTester extends  FlatSpec with Matchers {
+  implicit val p = config.Parameters.root((new MiniConfig).toInstance)
+  it should "Memory Controller tester" in {
+    chisel3.iotesters.Driver(() => new ReadTypTableEntry(0)(p)) {
+      c => new ReadTypTableEntryTests(c)
+    } should be(true)
+  }
+}
---
+++ src/test/scala/memory/deprecated/ReadWriteArbiter.scala
@@ -0,0 +1,225 @@
+package memory.deprecated
+
+/**
+  * Created by vnaveen0 on 9/7/17.
+  */
+
+import chisel3._
+import chisel3.testers._
+import chisel3.util._
+import config._
+import memory.ReadWriteArbiter
+
+class ReadWriteArbiterTests(c: => ReadWriteArbiter) (implicit p: config.Parameters)
+  extends BasicTester {
+
+  val dut = Module(c)
+  // -- IO ---
+  //    val ReadCacheReq = Decoupled(new CacheReq)
+  //    val ReadCacheResp = Flipped(Valid(new CacheResp))
+  //    val WriteCacheReq = Decoupled(new CacheReq)
+  //    val WriteCacheResp = Flipped(Valid(new CacheResp))
+  //    val CacheReq = Decoupled(new CacheReq)
+  //    val CacheResp = Flipped(Valid(new CacheRespT))
+
+  val sIdle :: sReq :: sDone :: Nil = Enum(3)
+  val state = RegInit(sIdle)
+  val isSt_R = RegInit(false.B)
+  val tag_R = RegInit(0.U)
+
+  val (testCnt, testDone) = Counter(sIdle === sReq, 2)
+  // -- --------------------------------------------------
+  dut.io.CacheReq.ready := true.B
+
+  switch(state) {
+    is(sIdle) {
+
+      printf("\n -----------------------------------------\n ")
+      printf(" state == Idle ")
+
+      //Store Req
+
+      when(dut.io.WriteCacheReq.ready) {
+
+        printf(" WriteCacheReq Ready ")
+        dut.io.WriteCacheReq.valid        := true.B
+        dut.io.WriteCacheReq.bits.addr    := 23.U
+        dut.io.WriteCacheReq.bits.data    := 4.U
+        dut.io.WriteCacheReq.bits.iswrite := true.B
+        dut.io.WriteCacheReq.bits.tag     := 1.U
+      }
+        .otherwise {
+          dut.io.WriteCacheReq.valid := false.B
+        }
+
+
+
+      when(dut.io.ReadCacheReq.ready) {
+
+        printf(" ReadCacheReq Ready ")
+        //Load Req
+        dut.io.ReadCacheReq.valid := true.B
+        dut.io.ReadCacheReq.bits.addr := 123.U
+        dut.io.ReadCacheReq.bits.data := 432232424.U //garbage
+        dut.io.ReadCacheReq.bits.iswrite := false.B
+        dut.io.ReadCacheReq.bits.tag := 1.U
+
+      }
+        .otherwise {
+          dut.io.ReadCacheReq.valid  := false.B
+        }
+
+
+      when(dut.io.ReadCacheReq.ready || dut.io.WriteCacheReq.ready) {
+        state := sReq
+        }
+
+      when(dut.io.CacheReq.valid) {
+        isSt_R := dut.io.CacheReq.bits.iswrite
+        tag_R := dut.io.CacheReq.bits.tag
+      }
+
+      printf("\n io.CacheReq.valid : %x ", dut.io.CacheReq.valid)
+      printf(" io.CacheReq.isSt : %x ", dut.io.CacheReq.bits.iswrite)
+      printf(" io.CacheReq.tag : %x \n", dut.io.CacheReq.bits.tag)
+
+      printf("ReadCacheReq.ready: %x ", dut.io.ReadCacheReq.ready)
+      printf("ReadCacheReq.valid: %x \n", dut.io.ReadCacheReq.valid)
+
+      printf("WriteCacheReq.ready: %x  ", dut.io.WriteCacheReq.ready)
+      printf("WriteCacheReq.valid: %x \n ", dut.io.WriteCacheReq.valid)
+
+    }
+
+    is(sReq) {
+      println(" \n state == sReq \n ")
+//      dut.io.CacheResp.data     := 34.U
+//      dut.io.CacheResp.isSt     := isSt_R // will depend on Ld/St
+//      dut.io.CacheResp.valid    := true.B
+//      dut.io.CacheResp.tag      := tag_R
+//      state := sDone
+    }
+
+    is(sDone) {
+
+      println(" \n state == sDone \n ")
+      state := sIdle
+      stop();
+      stop
+    }
+  }
+
+//  when(state === sIdle) {
+//    when(c.io.CacheReq.valid) {
+//      printf(" ^^^^^^^^^^ io.CacheReq.valid : ")
+//    }
+//    printf(" ^^^^^^^^^^ io.CacheReq.valid : 0x%x.", dut.io.CacheReq.valid)
+//    printf(s"ReadCacheReq.ready: %x ", dut.io.ReadCacheReq.ready)
+//    printf(s"ReadCacheReq.valid: %x ", dut.io.ReadCacheReq.valid)
+//
+//    printf(s"WriteCacheReq.ready: %x  ", dut.io.WriteCacheReq.ready)
+//    printf(s"WriteCacheReq.valid: %x  ", dut.io.WriteCacheReq.valid)
+//
+//
+//    printf(s" ^^^^^   ^^^ io.CacheReq.Valid: %x ", dut.io.CacheReq.valid)
+//    printf(s"io.CacheReq.addr: %x ", dut.io.CacheReq.bits.addr)
+//    printf(s"io.CacheReq.data: %x ", dut.io.CacheReq.bits.data)
+//    printf(s"io.CacheReq.tag:  %x ", dut.io.CacheReq.bits.tag)
+//    printf(s"io.CacheReq.mask: %x ", dut.io.CacheReq.bits.mask)
+//    printf(s"io.CacheReq.iswrite: %x ", dut.io.CacheReq.bits.iswrite)
+//
+//
+//    printf(s" Read/Write Response ----------")
+//    printf(s"io.write_resp.valid: %x ", dut.io.WriteCacheResp.valid)
+//    printf(s"io.read_resp.valid:  %x ", dut.io.ReadCacheResp.valid)
+//  }
+
+//  when(testDone) {
+//    stop();
+//    stop()
+//  }
+
+}
+
+////  switch(state) {
+////    is(sIdle) {
+////
+////      printf(" state == Idle ")
+//////      when(dut.io.CacheReq.ready) {
+//////
+//////        printf(" CacheReq Ready ")
+//////        //Load Req
+//////        dut.io.ReadCacheReq.valid         := true.B
+//////        dut.io.ReadCacheReq.bits.addr     := 123.U
+//////        dut.io.ReadCacheReq.bits.data     := 432232424.U //garbage
+//////        dut.io.ReadCacheReq.bits.iswrite  := false.B
+//////        dut.io.ReadCacheReq.bits.tag      := 1.U
+//////
+//////        //Store Req
+//////        dut.io.WriteCacheReq.valid        := true.B
+//////        dut.io.WriteCacheReq.bits.addr    := 23.U
+//////        dut.io.WriteCacheReq.bits.data    := 4.U
+//////        dut.io.WriteCacheReq.bits.iswrite := true.B
+//////        dut.io.WriteCacheReq.bits.tag     := 1.U
+//////        state := sReq
+//////      }
+//////        .otherwise {
+//////          dut.io.ReadCacheReq.valid  := false.B
+//////          dut.io.WriteCacheReq.valid := false.B
+//////        }
+////
+////    }
+////
+////    is(sReq) {
+////      println(s" ^^^^  ^^^ Receiving Response ")
+//////      dut.io.CacheResp.data     := 34.U
+//////      dut.io.CacheResp.isSt     := isSt_R // will depend on Ld/St
+//////      dut.io.CacheResp.valid    := true.B
+//////      dut.io.CacheResp.tag      := tag_R
+//////
+//////      state := sDone
+////    }
+////
+////    is(sDone) {
+//////      println(s"io.read_resp.data:    ${dut.io.ReadCacheResp.data}")
+//////      println(s"io.read_resp.tag:     ${dut.io.ReadCacheResp.tag}")
+//////
+//////      println(s"io.write_resp.data:  ${dut.io.WriteCacheResp.data}")
+//////      println(s"io.write_resp.tag:   ${dut.io.WriteCacheResp.tag}")
+//////
+//////      state := sIdle
+////      stop();
+////      stop
+////    }
+////  }
+//
+//
+//
+//  when(dut.io.CacheReq.valid) {
+//
+//    printf(s" ^^^^^   ^^^ io.CacheReq.Valid: ")
+//    printf(s" ^^^^^   ^^^ Sending Cache Req io.CacheReq.Valid: %x", dut.io.CacheReq.valid)
+//    isSt_R := dut.io.CacheReq.bits.iswrite
+//    tag_R  := dut.io.CacheReq.bits.tag
+//
+//  }
+//
+//
+
+//
+//
+//  // Connections
+//  dut.io.CacheReq.ready := (state === sIdle)
+//}
+
+
+
+
+
+
+class ReadWriteArbiterTester extends org.scalatest.FlatSpec {
+  implicit val p = config.Parameters.root((new MiniConfig).toInstance)
+  "ReadWriteArbiter" should "pass" in {
+    assert(TesterDriver execute (() => new ReadWriteArbiterTests(new ReadWriteArbiter())))
+  }
+}
