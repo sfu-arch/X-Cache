705976837dfc56e9558c66f754188d118483296a
Steve Margerm <smargerm@sfu.ca>
2018-03-17 14:54:59 -0700

    Added stub bitcast node.

---
+++ src/main/scala/node/BitCastNode.scala
@@ -0,0 +1,36 @@
+package node
+
+import chisel3._
+import chisel3.Module
+import config._
+import interfaces.{ControlBundle, DataBundle}
+import util._
+
+class BitCastNodeIO(val NumOuts: Int)
+                         (implicit p: Parameters) extends CoreBundle()(p) {
+
+    //Input for Bitcast
+    val Input = Flipped(Decoupled(new DataBundle()))
+    //val Input = Flipped(Decoupled(UInt(src.W)))
+
+    //Enabl signal
+    val enable = Flipped(Decoupled(Bool()))
+
+    //Output of the input (Zexted version)
+    val Out = Vec(NumOuts, Decoupled(new DataBundle()))
+    //val Out = Output(Vec(nout, Decoupled(UInt(des.W))))
+
+}
+
+class BitCastNode(val NumOuts: Int,val ID : Int)(implicit val p: Parameters)
+  extends Module with CoreParams{
+
+  lazy val io = IO(new BitCastNodeIO(NumOuts))
+
+  for(i <- 0 until NumOuts){
+    io.Out(i) <> io.Input
+  }
+  io.enable.ready := true.B
+
+}
+
