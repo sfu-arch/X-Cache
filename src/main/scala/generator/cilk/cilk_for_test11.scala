2288f2b62330fbb4fd126d86ab52170de31e07e8
Steve Margerm <smargerm@sfu.ca>
2018-03-23 16:57:06 -0700

    Added cilk_for_test11

---
+++ src/main/scala/generator/cilk/cilk_for_test11.scala
@@ -0,0 +1,634 @@
+package dataflow
+
+import chisel3._
+import chisel3.util._
+import chisel3.Module
+import chisel3.testers._
+import chisel3.iotesters._
+import org.scalatest.{FlatSpec, Matchers}
+import muxes._
+import config._
+import control._
+import util._
+import interfaces._
+import regfile._
+import memory._
+import stack._
+import arbiters._
+import loop._
+import accel._
+import node._
+import junctions._
+
+
+/**
+  * This Object should be initialized at the first step
+  * It contains all the transformation from indices to their module's name
+  */
+
+object Data_cilk_for_test11_FlowParam{
+
+  val bb_entry_pred = Map(
+    "active" -> 0
+  )
+
+
+  val bb_pfor_detach_preheader_pred = Map(
+    "br1" -> 0
+  )
+
+
+  val bb_pfor_cond_cleanup_pred = Map(
+    "br1" -> 0,
+    "br3" -> 1
+  )
+
+
+  val bb_pfor_detach_pred = Map(
+    "br2" -> 0,
+    "br10" -> 1
+  )
+
+
+  val bb_pfor_cond_cleanup_loopexit_pred = Map(
+    "br10" -> 0
+  )
+
+
+  val br1_brn_bb = Map(
+    "bb_pfor_detach_preheader" -> 0,
+    "bb_pfor_cond_cleanup" -> 1
+  )
+
+
+  val br2_brn_bb = Map(
+    "bb_pfor_detach" -> 0
+  )
+
+
+  val br3_brn_bb = Map(
+    "bb_pfor_cond_cleanup" -> 0
+  )
+
+
+  val br10_brn_bb = Map(
+    "bb_pfor_cond_cleanup_loopexit" -> 0,
+    "bb_pfor_detach" -> 1
+  )
+
+
+  val bb_pfor_inc_pred = Map(
+    "detach7" -> 0
+  )
+
+
+  val bb_offload_pfor_body_pred = Map(
+    "detach7" -> 0
+  )
+
+
+  val detach7_brn_bb = Map(
+    "bb_offload_pfor_body" -> 0,
+    "bb_pfor_inc" -> 1
+  )
+
+
+  val bb_entry_activate = Map(
+    "icmp0" -> 0,
+    "br1" -> 1
+  )
+
+
+  val bb_pfor_detach_preheader_activate = Map(
+    "br2" -> 0
+  )
+
+
+  val bb_pfor_cond_cleanup_loopexit_activate = Map(
+    "br3" -> 0
+  )
+
+
+  val bb_pfor_cond_cleanup_activate = Map(
+    "sync4" -> 0
+  )
+
+
+  val bb_pfor_end_continue_activate = Map(
+    "ret5" -> 0
+  )
+
+
+  val bb_pfor_detach_activate = Map(
+    "phi6" -> 0,
+    "detach7" -> 1
+  )
+
+
+  val bb_pfor_inc_activate = Map(
+    "add8" -> 0,
+    "icmp9" -> 1,
+    "br10" -> 2
+  )
+
+
+  val bb_offload_pfor_body_activate = Map(
+    "call11" -> 0,
+    "reattach12" -> 1
+  )
+
+
+  val phi6_phi_in = Map(
+    "add8" -> 1,
+    "const_1" -> 0
+  )
+
+
+  //  %cmp17 = icmp sgt i32 %n, 0, !UID !2, !ScalaLabel !3
+  val icmp0_in = Map(
+    "field1" -> 0
+  )
+
+
+  //  br i1 %cmp17, label %pfor.detach.preheader, label %pfor.cond.cleanup, !UID !4, !BB_UID !5, !ScalaLabel !6
+  val br1_in = Map(
+    "icmp0" -> 0
+  )
+
+
+  //  sync label %pfor.end.continue, !UID !13, !BB_UID !14, !ScalaLabel !15
+  val sync4_in = Map(
+    "" -> 0
+  )
+
+
+  //  ret i32 1, !UID !16, !BB_UID !17, !ScalaLabel !18
+  val ret5_in = Map(
+
+  )
+
+
+  //  %i.018 = phi i32 [ %inc, %pfor.inc ], [ 0, %pfor.detach.preheader ], !UID !19, !ScalaLabel !20
+  val phi6_in = Map(
+    "add8" -> 0
+  )
+
+
+  //  detach label %offload.pfor.body, label %pfor.inc, !UID !21, !BB_UID !22, !ScalaLabel !23
+  val detach7_in = Map(
+    "" -> 1,
+    "" -> 2
+  )
+
+
+  //  %inc = add nuw nsw i32 %i.018, 1, !UID !24, !ScalaLabel !25
+  val add8_in = Map(
+    "phi6" -> 0
+  )
+
+
+  //  %exitcond = icmp eq i32 %inc, %n, !UID !26, !ScalaLabel !27
+  val icmp9_in = Map(
+    "add8" -> 1,
+    "field1" -> 1
+  )
+
+
+  //  br i1 %exitcond, label %pfor.cond.cleanup.loopexit, label %pfor.detach, !llvm.loop !28, !UID !30, !BB_UID !31, !ScalaLabel !32
+  val br10_in = Map(
+    "icmp9" -> 0
+  )
+
+
+  //  call void @cilk_for_test11_detach1(%struct.Edge* %E, i32 %i.018), !UID !33, !ScalaLabel !34
+  val call11_in = Map(
+    "field0" -> 0,
+    "phi6" -> 1,
+    "" -> 3
+  )
+
+
+  //  reattach label %pfor.inc, !UID !35, !BB_UID !36, !ScalaLabel !37
+  val reattach12_in = Map(
+    "" -> 4
+  )
+
+
+}
+
+
+
+
+  /* ================================================================== *
+   *                   PRINTING PORTS DEFINITION                        *
+   * ================================================================== */
+
+
+abstract class cilk_for_test11DFIO(implicit val p: Parameters) extends Module with CoreParams {
+  val io = IO(new Bundle {
+    val in = Flipped(Decoupled(new Call(List(32,32))))
+    val call11_out = Decoupled(new Call(List(32,32)))
+    val call11_in = Flipped(Decoupled(new Call(List(32))))
+    val CacheResp = Flipped(Valid(new CacheRespT))
+    val CacheReq = Decoupled(new CacheReq)
+    val out = Decoupled(new Call(List(32)))
+  })
+}
+
+
+
+
+  /* ================================================================== *
+   *                   PRINTING MODULE DEFINITION                       *
+   * ================================================================== */
+
+
+class cilk_for_test11DF(implicit p: Parameters) extends cilk_for_test11DFIO()(p) {
+
+
+
+  /* ================================================================== *
+   *                   PRINTING MEMORY SYSTEM                           *
+   * ================================================================== */
+
+
+	val StackPointer = Module(new Stack(NumOps = 1))
+
+	val RegisterFile = Module(new TypeStackFile(ID=0,Size=32,NReads=2,NWrites=2)
+		            (WControl=new WriteMemoryController(NumOps=2,BaseSize=2,NumEntries=2))
+		            (RControl=new ReadMemoryController(NumOps=2,BaseSize=2,NumEntries=2)))
+
+	val CacheMem = Module(new UnifiedController(ID=0,Size=32,NReads=2,NWrites=2)
+		            (WControl=new WriteMemoryController(NumOps=2,BaseSize=2,NumEntries=2))
+		            (RControl=new ReadMemoryController(NumOps=2,BaseSize=2,NumEntries=2))
+		            (RWArbiter=new ReadWriteArbiter()))
+
+  io.CacheReq <> CacheMem.io.CacheReq
+  CacheMem.io.CacheResp <> io.CacheResp
+
+  val InputSplitter = Module(new SplitCall(List(32,32)))
+  InputSplitter.io.In <> io.in
+
+  val field1_expand = Module(new ExpandNode(NumOuts=2,ID=102)(new DataBundle))
+  field1_expand.io.enable.valid := true.B
+  field1_expand.io.enable.bits.control := true.B
+  field1_expand.io.InData <> InputSplitter.io.Out.data("field1")
+
+
+  /* ================================================================== *
+   *                   PRINTING LOOP HEADERS                            *
+   * ================================================================== */
+
+
+  val loop_L_0_liveIN_0 = Module(new LiveInNode(NumOuts = 1, ID = 0))
+  val loop_L_0_liveIN_1 = Module(new LiveInNode(NumOuts = 1, ID = 0))
+
+
+
+
+  /* ================================================================== *
+   *                   PRINTING BASICBLOCK NODES                        *
+   * ================================================================== */
+
+
+  //Initializing BasicBlocks: 
+
+  val bb_entry = Module(new BasicBlockNoMaskNode(NumInputs = 1, NumOuts = 2, BID = 0))
+
+  val bb_pfor_detach_preheader = Module(new BasicBlockNoMaskNode(NumInputs = 1, NumOuts = 1, BID = 1))
+
+  val bb_pfor_cond_cleanup_loopexit = Module(new BasicBlockNoMaskNode(NumInputs = 1, NumOuts = 3, BID = 2))
+
+  val bb_pfor_cond_cleanup = Module(new BasicBlockNoMaskNode(NumInputs = 2, NumOuts = 1, BID = 3))
+
+  val bb_pfor_end_continue = Module(new BasicBlockNoMaskNode(NumInputs = 1, NumOuts = 1, BID = 4))
+
+  val bb_pfor_detach = Module(new  LoopHead(NumOuts = 2, NumPhi = 1, BID = 5))
+
+  val bb_pfor_inc = Module(new BasicBlockNoMaskNode(NumInputs = 2, NumOuts = 3, BID = 6))
+
+  val bb_offload_pfor_body = Module(new BasicBlockNoMaskNode(NumInputs = 1, NumOuts = 2, BID = 7))
+
+
+
+
+
+
+  /* ================================================================== *
+   *                   PRINTING INSTRUCTION NODES                       *
+   * ================================================================== */
+
+
+  //Initializing Instructions: 
+
+  // [BasicBlock]  entry:
+
+  //  %cmp17 = icmp sgt i32 %n, 0, !UID !2, !ScalaLabel !3
+  val icmp0 = Module (new IcmpNode(NumOuts = 1, ID = 0, opCode = "UGT")(sign=false))
+
+
+  //  br i1 %cmp17, label %pfor.detach.preheader, label %pfor.cond.cleanup, !UID !4, !BB_UID !5, !ScalaLabel !6
+  val br1 = Module (new CBranchNode(ID = 1))
+
+  // [BasicBlock]  pfor.detach.preheader:
+
+  //  br label %pfor.detach, !UID !7, !BB_UID !8, !ScalaLabel !9
+  val br2 = Module (new UBranchNode(ID = 2))
+
+  // [BasicBlock]  pfor.cond.cleanup.loopexit:
+
+  //  br label %pfor.cond.cleanup, !UID !10, !BB_UID !11, !ScalaLabel !12
+  val br3 = Module (new UBranchNode(ID = 3))
+
+  // [BasicBlock]  pfor.cond.cleanup:
+
+  //  sync label %pfor.end.continue, !UID !13, !BB_UID !14, !ScalaLabel !15
+  val sync4 = Module(new Sync(ID = 4, NumOuts = 1, NumInc = 1, NumDec = 1))
+
+  // [BasicBlock]  pfor.end.continue:
+
+  //  ret i32 1, !UID !16, !BB_UID !17, !ScalaLabel !18
+  val ret5 = Module(new RetNode(NumPredIn=1, retTypes=List(32), ID=5))
+
+  // [BasicBlock]  pfor.detach:
+
+  //  %i.018 = phi i32 [ %inc, %pfor.inc ], [ 0, %pfor.detach.preheader ], !UID !19, !ScalaLabel !20
+  val phi6 = Module (new PhiNode(NumInputs = 2, NumOuts = 2, ID = 6))
+
+
+  //  detach label %offload.pfor.body, label %pfor.inc, !UID !21, !BB_UID !22, !ScalaLabel !23
+  val detach7 = Module(new Detach(ID = 7))
+
+  // [BasicBlock]  pfor.inc:
+
+  //  %inc = add nuw nsw i32 %i.018, 1, !UID !24, !ScalaLabel !25
+  val add8 = Module (new ComputeNode(NumOuts = 2, ID = 8, opCode = "add")(sign=false))
+
+
+  //  %exitcond = icmp eq i32 %inc, %n, !UID !26, !ScalaLabel !27
+  val icmp9 = Module (new IcmpNode(NumOuts = 1, ID = 9, opCode = "EQ")(sign=false))
+
+
+  //  br i1 %exitcond, label %pfor.cond.cleanup.loopexit, label %pfor.detach, !llvm.loop !28, !UID !30, !BB_UID !31, !ScalaLabel !32
+  val br10 = Module (new CBranchNode(ID = 10))
+
+  // [BasicBlock]  offload.pfor.body:
+
+  //  call void @cilk_for_test11_detach1(%struct.Edge* %E, i32 %i.018), !UID !33, !ScalaLabel !34
+  val call11 = Module(new CallNode(ID=11,argTypes=List(32,32),retTypes=List(32)))
+
+
+  //  reattach label %pfor.inc, !UID !35, !BB_UID !36, !ScalaLabel !37
+  val reattach12 = Module(new Reattach(NumPredIn=1, ID=12))
+
+
+
+
+
+  /* ================================================================== *
+   *                   INITIALIZING PARAM                               *
+   * ================================================================== */
+
+
+  /**
+    * Instantiating parameters
+    */
+  val param = Data_cilk_for_test11_FlowParam
+
+
+
+  /* ================================================================== *
+   *                   CONNECTING BASIC BLOCKS TO PREDICATE INSTRUCTIONS*
+   * ================================================================== */
+
+
+  /**
+     * Connecting basic blocks to predicate instructions
+     */
+
+
+  bb_entry.io.predicateIn <> InputSplitter.io.Out.enable
+
+  /**
+    * Connecting basic blocks to predicate instructions
+    */
+
+  //Connecting br1 to bb_pfor_detach_preheader
+  bb_pfor_detach_preheader.io.predicateIn <> br1.io.Out(param.br1_brn_bb("bb_pfor_detach_preheader"))
+
+
+  //Connecting br1 to bb_pfor_cond_cleanup
+  bb_pfor_cond_cleanup.io.predicateIn <> br1.io.Out(param.br1_brn_bb("bb_pfor_cond_cleanup"))
+
+
+  //Connecting br2 to bb_pfor_detach
+  bb_pfor_detach.io.activate <> br2.io.Out(param.br2_brn_bb("bb_pfor_detach"))
+
+
+  //Connecting br3 to bb_pfor_cond_cleanup
+  bb_pfor_cond_cleanup.io.predicateIn <> br3.io.Out(param.br3_brn_bb("bb_pfor_cond_cleanup"))
+
+
+  //Connecting br10 to bb_pfor_cond_cleanup_loopexit
+  bb_pfor_cond_cleanup_loopexit.io.predicateIn <> br10.io.Out(param.br10_brn_bb("bb_pfor_cond_cleanup_loopexit"))
+
+
+  //Connecting br10 to bb_pfor_detach
+  bb_pfor_detach.io.loopBack <> br10.io.Out(param.br10_brn_bb("bb_pfor_detach"))
+
+
+  //Connecting detach7 to bb_offload_pfor_body
+  bb_offload_pfor_body.io.predicateIn <> detach7.io.Out(param.detach7_brn_bb("bb_offload_pfor_body"))
+
+
+  //Connecting detach7 to bb_pfor_inc
+  bb_pfor_inc.io.predicateIn <> detach7.io.Out(param.detach7_brn_bb("bb_pfor_inc"))
+
+
+
+
+  /* ================================================================== *
+   *                   CONNECTING BASIC BLOCKS TO INSTRUCTIONS          *
+   * ================================================================== */
+
+
+  /**
+    * Wiring enable signals to the instructions
+    */
+
+  icmp0.io.enable <> bb_entry.io.Out(param.bb_entry_activate("icmp0"))
+
+  br1.io.enable <> bb_entry.io.Out(param.bb_entry_activate("br1"))
+
+
+
+  br2.io.enable <> bb_pfor_detach_preheader.io.Out(param.bb_pfor_detach_preheader_activate("br2"))
+
+
+
+  br3.io.enable <> bb_pfor_cond_cleanup_loopexit.io.Out(param.bb_pfor_cond_cleanup_loopexit_activate("br3"))
+
+  loop_L_0_liveIN_0.io.enable <> bb_pfor_cond_cleanup_loopexit.io.Out(1)
+  loop_L_0_liveIN_1.io.enable <> bb_pfor_cond_cleanup_loopexit.io.Out(2)
+
+
+
+
+  sync4.io.enable <> bb_pfor_cond_cleanup.io.Out(param.bb_pfor_cond_cleanup_activate("sync4"))
+
+
+
+  ret5.io.enable <> bb_pfor_end_continue.io.Out(param.bb_pfor_end_continue_activate("ret5"))
+
+
+
+  phi6.io.enable <> bb_pfor_detach.io.Out(param.bb_pfor_detach_activate("phi6"))
+
+  detach7.io.enable <> bb_pfor_detach.io.Out(param.bb_pfor_detach_activate("detach7"))
+
+  bb_pfor_end_continue.io.predicateIn <> sync4.io.Out(0) // added manually
+
+
+  add8.io.enable <> bb_pfor_inc.io.Out(param.bb_pfor_inc_activate("add8"))
+
+  icmp9.io.enable <> bb_pfor_inc.io.Out(param.bb_pfor_inc_activate("icmp9"))
+
+  br10.io.enable <> bb_pfor_inc.io.Out(param.bb_pfor_inc_activate("br10"))
+
+
+
+  call11.io.In.enable <> bb_offload_pfor_body.io.Out(param.bb_offload_pfor_body_activate("call11"))
+
+
+  // Manual fix.  Reattach should only depend on its increment/decr inputs. If it is connected to the BB
+  // Enable it will stall the loop
+//  reattach12.io.enable <> bb_offload_pfor_body.io.Out(param.bb_offload_pfor_body_activate("reattach12"))
+  reattach12.io.enable.enq(ControlBundle.active())  // always enabled
+  bb_offload_pfor_body.io.Out(param.bb_offload_pfor_body_activate("reattach12")).ready := true.B
+
+
+
+
+  /* ================================================================== *
+   *                   CONNECTING LOOPHEADERS                           *
+   * ================================================================== */
+
+
+  // Connecting function argument to the loop header
+  //%struct.Edge* %E
+  loop_L_0_liveIN_0.io.InData <> InputSplitter.io.Out.data("field0")
+
+  // Connecting function argument to the loop header
+  //i32 %n
+  loop_L_0_liveIN_1.io.InData <> field1_expand.io.Out(0)
+
+
+
+  /* ================================================================== *
+   *                   DUMPING PHI NODES                                *
+   * ================================================================== */
+
+
+  /**
+    * Connecting PHI Masks
+    */
+  //Connect PHI node
+
+  phi6.io.InData(param.phi6_phi_in("add8")) <> add8.io.Out(param.phi6_in("add8"))
+
+  phi6.io.InData(param.phi6_phi_in("const_1")).bits.data := 0.U
+  phi6.io.InData(param.phi6_phi_in("const_1")).bits.predicate := true.B
+  phi6.io.InData(param.phi6_phi_in("const_1")).valid := true.B
+
+  /**
+    * Connecting PHI Masks
+    */
+  //Connect PHI node
+
+  phi6.io.Mask <> bb_pfor_detach.io.MaskBB(0)
+
+
+
+  /* ================================================================== *
+   *                   DUMPING DATAFLOW                                 *
+   * ================================================================== */
+
+
+  /**
+    * Connecting Dataflow signals
+    */
+
+  // Wiring Binary instruction to the function argument
+  icmp0.io.LeftIO <> field1_expand.io.Out(1)
+
+  // Wiring constant
+  icmp0.io.RightIO.bits.data := 0.U
+  icmp0.io.RightIO.bits.predicate := true.B
+  icmp0.io.RightIO.valid := true.B
+
+  // Wiring Branch instruction
+  br1.io.CmpIO <> icmp0.io.Out(param.br1_in("icmp0"))
+
+
+  // Reattach (Manual add)
+  reattach12.io.predicateIn(0) <> call11.io.Out.data("field0")
+
+  // Sync (Manual add)
+  sync4.io.incIn(0) <> detach7.io.Out(2)
+  sync4.io.decIn(0) <> reattach12.io.Out(0)
+
+  // Wiring return instruction
+  ret5.io.predicateIn(0).bits.control := true.B
+  ret5.io.predicateIn(0).bits.taskID := 0.U
+  ret5.io.predicateIn(0).valid := true.B
+  ret5.io.In.data("field0").bits.data := 1.U
+  ret5.io.In.data("field0").bits.predicate := true.B
+  ret5.io.In.data("field0").valid := true.B
+  io.out <> ret5.io.Out
+
+
+  // Wiring instructions
+  add8.io.LeftIO <> phi6.io.Out(param.add8_in("phi6"))
+
+  // Wiring constant
+  add8.io.RightIO.bits.data := 1.U
+  add8.io.RightIO.bits.predicate := true.B
+  add8.io.RightIO.valid := true.B
+
+  // Wiring instructions
+  icmp9.io.LeftIO <> add8.io.Out(param.icmp9_in("add8"))
+
+  // Wiring Binary instruction to the loop header
+  icmp9.io.RightIO <> loop_L_0_liveIN_1.io.Out(0)  // manual
+
+  // Wiring Branch instruction
+  br10.io.CmpIO <> icmp9.io.Out(param.br10_in("icmp9"))
+
+  // Wiring Call to I/O
+  io.call11_out <> call11.io.callOut
+  call11.io.retIn <> io.call11_in
+  call11.io.Out.enable.ready := true.B // Manual fix
+  // Wiring Call instruction to the loop header
+  call11.io.In.data("field0") <> loop_L_0_liveIN_0.io.Out(param.call11_in("field0"))
+
+  // Wiring Call instruction to the loop header
+  call11.io.In.data("field1") <> phi6.io.Out(param.call11_in("phi6"))
+
+
+
+}
+
+import java.io.{File, FileWriter}
+object cilk_for_test11Main extends App {
+  val dir = new File("RTL/cilk_for_test11") ; dir.mkdirs
+  implicit val p = config.Parameters.root((new MiniConfig).toInstance)
+  val chirrtl = firrtl.Parser.parse(chisel3.Driver.emit(() => new cilk_for_test11DF()))
+
+  val verilogFile = new File(dir, s"${chirrtl.main}.v")
+  val verilogWriter = new FileWriter(verilogFile)
+  val compileResult = (new firrtl.VerilogCompiler).compileAndEmit(firrtl.CircuitState(chirrtl, firrtl.ChirrtlForm))
+  val compiledStuff = compileResult.getEmittedCircuit
+  verilogWriter.write(compiledStuff.value)
+  verilogWriter.close()
+}
+
---
+++ src/test/scala/generator/cilk/cilk_for_test11_test.scala
@@ -0,0 +1,264 @@
+package dataflow
+
+import chisel3._
+import chisel3.util._
+import chisel3.Module
+import chisel3.testers._
+import chisel3.iotesters._
+import org.scalatest.{FlatSpec, Matchers}
+import muxes._
+import config._
+import control._
+import util._
+import interfaces._
+import regfile._
+import memory._
+import stack._
+import arbiters._
+import loop._
+import accel._
+import node._
+
+
+class cilk_for_test11MainIO(implicit val p: Parameters)  extends Module with CoreParams with CacheParams {
+  val io = IO( new CoreBundle {
+    val in = Flipped(Decoupled(new Call(List(32,32,32))))
+    val addr = Input(UInt(nastiXAddrBits.W))  // Initialization address
+    val din  = Input(UInt(nastiXDataBits.W))  // Initialization data
+    val write = Input(Bool())                 // Initialization write strobe
+    val dout = Output(UInt(nastiXDataBits.W))
+    val out = Decoupled(new Call(List(32)))
+  })
+}
+
+class cilk_for_test11MainDirect(implicit p: Parameters) extends cilk_for_test11MainIO {
+
+  val cache = Module(new Cache)            // Simple Nasti Cache
+  val memModel = Module(new NastiMemSlave) // Model of DRAM to connect to Cache
+  val memCopy = Mem(1024, UInt(32.W))      // Local memory just to keep track of writes to cache for validation
+
+  // Store a copy of all data written to the cache.  This is done since the cache isn't
+  // 'write through' to the memory model and we have no easy way of reading the
+  // cache contents from the testbench.
+  when(cache.io.cpu.req.valid && cache.io.cpu.req.bits.iswrite) {
+    memCopy.write((cache.io.cpu.req.bits.addr>>2).asUInt(), cache.io.cpu.req.bits.data)
+  }
+  io.dout := memCopy.read((io.addr>>2).asUInt())
+
+  // Connect the wrapper I/O to the memory model initialization interface so the
+  // test bench can write contents at start.
+  memModel.io.nasti <> cache.io.nasti
+  memModel.io.init.bits.addr := io.addr
+  memModel.io.init.bits.data := io.din
+  memModel.io.init.valid := io.write
+  cache.io.cpu.abort := false.B
+
+  // Wire up the cache and modules under test.
+  val cilk_for_test11_detach = Module(new cilk_for_test11_detach1DF())
+  val cilk_for_test11 = Module(new cilk_for_test11DF())
+
+  cache.io.cpu.req <> cilk_for_test11_detach.io.CacheReq
+  cilk_for_test11_detach.io.CacheResp <> cache.io.cpu.resp
+  cilk_for_test11.io.in <> io.in
+  cilk_for_test11_detach.io.in <> cilk_for_test11.io.call11_out
+  cilk_for_test11.io.call11_in <> cilk_for_test11_detach.io.out
+  io.out <> cilk_for_test11.io.out
+
+}
+
+class cilk_for_test11MainTM(implicit p: Parameters) extends cilk_for_test11MainIO  {
+
+  val cache = Module(new Cache)            // Simple Nasti Cache
+  val memModel = Module(new NastiMemSlave) // Model of DRAM to connect to Cache
+  val memCopy = Mem(1024, UInt(32.W))      // Local memory just to keep track of writes to cache for validation
+
+  // Store a copy of all data written to the cache.  This is done since the cache isn't
+  // 'write through' to the memory model and we have no easy way of reading the
+  // cache contents from the testbench.
+  when(cache.io.cpu.req.valid && cache.io.cpu.req.bits.iswrite) {
+    memCopy.write((cache.io.cpu.req.bits.addr>>2).asUInt(), cache.io.cpu.req.bits.data)
+  }
+  io.dout := memCopy.read((io.addr>>2).asUInt())
+
+  // Connect the wrapper I/O to the memory model initialization interface so the
+  // test bench can write contents at start.
+  memModel.io.nasti <> cache.io.nasti
+  memModel.io.init.bits.addr := io.addr
+  memModel.io.init.bits.data := io.din
+  memModel.io.init.valid := io.write
+  cache.io.cpu.abort := false.B
+
+  // Wire up the cache, TM, and modules under test.
+
+  val children = 2
+  val TaskControllerModule = Module(new TaskController(List(32,32,32,32), List(32), 1, children))
+  val cilk_for_test11 = Module(new cilk_for_test11DF())
+
+  val cilk_for_test11_detach = for (i <- 0 until children) yield {
+    val foo = Module(new cilk_for_test11_detach1DF())
+    foo
+  }
+
+  // Ugly hack to merge requests from two children.  "ReadWriteArbiter" merges two
+  // requests ports of any type.  Read or write is irrelevant.
+  val CacheArbiter = Module(new CacheArbiter(children))
+  for (i <- 0 until children) {
+    CacheArbiter.io.cpu.CacheReq(i) <> cilk_for_test11_detach(i).io.CacheReq
+    cilk_for_test11_detach(i).io.CacheResp <> CacheArbiter.io.cpu.CacheResp(i)
+  }
+  cache.io.cpu.req <> CacheArbiter.io.cache.CacheReq
+  CacheArbiter.io.cache.CacheResp <> cache.io.cpu.resp
+
+  // tester to cilk_for_test11
+  cilk_for_test11.io.in <> io.in
+
+  // cilk_for_test11 to task controller
+  TaskControllerModule.io.parentIn(0) <> cilk_for_test11.io.call11_out
+
+  // task controller to sub-task cilk_for_test11_detach
+  for (i <- 0 until children ) {
+    cilk_for_test11_detach(i).io.in <> TaskControllerModule.io.childOut(i)
+    TaskControllerModule.io.childIn(i) <> cilk_for_test11_detach(i).io.out
+  }
+
+  // Task controller to cilk_for_test11
+  cilk_for_test11.io.call11_in <> TaskControllerModule.io.parentOut(0)
+
+  // cilk_for_test11 to tester
+  io.out <> cilk_for_test11.io.out
+
+}
+
+class cilk_for_test11Test01[T <: cilk_for_test11MainIO](c: T) extends PeekPokeTester(c) {
+  val inAddrVec = List.range(0, 144, 4)
+  val inDataVec = List(0,0, 0,1, 0,2, 1,0, 1,1 ,1,2, 2,0,  2,1 ,2,2,
+                       1,1, 2,3, 3,5, 5,4, 6,6, 7,8, 9,7, 10,9, 2,2)
+
+  poke(c.io.addr, 0.U)
+  poke(c.io.din, 0.U)
+  poke(c.io.write, false.B)
+  var i = 0
+
+  // Write initial contents to the memory model.
+  for(i <- 0 until inDataVec.length) {
+    poke(c.io.addr, inAddrVec(i))
+    poke(c.io.din, inDataVec(i))
+    poke(c.io.write, true.B)
+    step(1)
+  }
+  poke(c.io.write, false.B)
+  step(1)
+
+  // Initializing the signals
+  poke(c.io.in.bits.enable.control, false.B)
+  poke(c.io.in.valid, false.B)
+  poke(c.io.in.bits.data("field0").data, 0.U)
+  poke(c.io.in.bits.data("field0").taskID, 5.U)
+  poke(c.io.in.bits.data("field0").predicate, false.B)
+  poke(c.io.in.bits.data("field1").data, 0.U)
+  poke(c.io.in.bits.data("field1").taskID, 5.U)
+  poke(c.io.in.bits.data("field1").predicate, false.B)
+  poke(c.io.out.ready, false.B)
+  step(1)
+  poke(c.io.in.bits.enable.control, true.B)
+  poke(c.io.in.valid, true.B)
+  poke(c.io.in.bits.data("field0").data, 0.U)    // Array a[] base address
+  poke(c.io.in.bits.data("field0").predicate, true.B)
+  poke(c.io.in.bits.data("field1").data, 18.U)   // Array b[] base address
+  poke(c.io.in.bits.data("field1").predicate, true.B)
+  poke(c.io.out.ready, true.B)
+  step(1)
+  poke(c.io.in.bits.enable.control, false.B)
+  poke(c.io.in.valid, false.B)
+  poke(c.io.in.bits.data("field0").data, 0)
+  poke(c.io.in.bits.data("field0").predicate, false.B)
+  poke(c.io.in.bits.data("field1").data, 0.U)
+  poke(c.io.in.bits.data("field1").predicate, false.B)
+
+  step(1)
+
+  // NOTE: Don't use assert().  It seems to terminate the writing of VCD files
+  // early (before the error) which makes debugging very difficult. Check results
+  // using if() and fail command.
+  var time = 0
+  var result = false
+  while (time < 500) {
+    time += 1
+    step(1)
+    if (peek(c.io.out.valid) == 1 &&
+      peek(c.io.out.bits.data("field0").predicate) == 1
+      ) {
+      result = true
+      val data = peek(c.io.out.bits.data("field0").data)
+      if (data != 1) {
+        println(Console.RED + s"*** Incorrect result received. Got $data. Hoping for 1" + Console.RESET)
+        fail
+      } else {
+        println(Console.BLUE + s"*** Correct return result received. Run time: $time cycles." + Console.RESET)
+      }
+    }
+  }
+
+  /*
+  //  Peek into the CopyMem to see if the expected data is written back to the Cache
+  var valid_data = true
+  for(i <- 0 until outDataVec.length) {
+    poke(c.io.addr, outAddrVec(i))
+    step(1)
+    val data = peek(c.io.dout)
+    if (data != outDataVec(i).toInt) {
+      println(Console.RED + s"*** Incorrect data received. Got $data. Hoping for ${outDataVec(i).toInt}" + Console.RESET)
+      fail
+      valid_data = false
+    }
+  }
+  if (valid_data) {
+    println(Console.BLUE + "*** Correct data written back." + Console.RESET)
+  }
+*/
+
+  if(!result) {
+    println(Console.RED + "*** Timeout." + Console.RESET)
+    fail
+  }
+}
+
+class cilk_for_test11Tester1 extends FlatSpec with Matchers {
+  implicit val p = config.Parameters.root((new MiniConfig).toInstance)
+  it should "Check that cilk_for_test11 works correctly." in {
+    // iotester flags:
+    // -ll  = log level <Error|Warn|Info|Debug|Trace>
+    // -tbn = backend <firrtl|verilator|vcs>
+    // -td  = target directory
+    // -tts = seed for RNG
+    chisel3.iotesters.Driver.execute(
+     Array(
+       // "-ll", "Info",
+       "-tbn", "verilator",
+       "-td", "test_run_dir",
+       "-tts", "0001"),
+     () => new cilk_for_test11MainDirect()) {
+     c => new cilk_for_test11Test01(c)
+    } should be(true)
+  }
+}
+
+class cilk_for_test11Tester2 extends FlatSpec with Matchers {
+  implicit val p = config.Parameters.root((new MiniConfig).toInstance)
+  // iotester flags:
+  // -ll  = log level <Error|Warn|Info|Debug|Trace>
+  // -tbn = backend <firrtl|verilator|vcs>
+  // -td  = target directory
+  // -tts = seed for RNG
+  it should "Check that cilk_for_test02 works when called via task manager." in {
+    chisel3.iotesters.Driver.execute(
+      Array(
+        // "-ll", "Info",
+        "-tbn", "verilator",
+        "-td", "test_run_dir",
+        "-tts", "0001"),
+      () => new cilk_for_test11MainTM()) {
+      c => new cilk_for_test11Test01(c)
+    } should be(true)
+  }
+}
