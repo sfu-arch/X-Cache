11495849e072e4243b3c33de7e36c87799151e54
Arrvindh Shriraman <ashriram@ashriram-laptop.lan>
2018-05-16 16:14:27 -0700

    Test updated for FPDiv

--- src/test/scala/FPU/FPDiv.scala
+++ src/test/scala/FPU/FPDiv.scala
@@ -26,16 +26,15 @@ class FPDivNodeTester(c: FPDivSqrtNode) extends PeekPokeTester(c) {
 
 
     poke(c.io.a.valid, true)
-    poke(c.io.a.bits.data, 12)
+    poke(c.io.a.bits.data,0x42800000)
+    poke(c.io.a.bits.taskID, 22)
     poke(c.io.a.bits.predicate, true)
     poke(c.io.b.valid, true)
-    poke(c.io.b.bits.data, t+1)
+    poke(c.io.b.bits.data, 0x41800000)
     poke(c.io.b.bits.predicate,true)
+    poke(c.io.b.bits.taskID, 22)
     poke(c.io.enable.bits.control,true)
     poke(c.io.enable.valid,true)
-
-    step(1)
-    step(1)
     step(1)
     step(1)
 
@@ -44,10 +43,10 @@ class FPDivNodeTester(c: FPDivSqrtNode) extends PeekPokeTester(c) {
     step(1)
     step(1)
     step(1)
-
+    print(s"t: ${t} io.field0: ${peek(c.io.FUReq.bits.data("field0"))} io.field1: ${peek(c.io.FUReq.bits.data("field1"))} io.field2: ${peek(c.io.FUReq.bits.data("field2"))} \n")
     poke(c.io.FUResp.data,100)
     poke(c.io.FUResp.valid,true)
-          // printf(s"t: ${t}  io.Out: ${peek(c.io.Out(0))} \n")
+
 
     step(1)
     step(1)
@@ -62,7 +61,7 @@ class FPDivNodeTester(c: FPDivSqrtNode) extends PeekPokeTester(c) {
 class FPDivNodeTests extends  FlatSpec with Matchers {
   implicit val p = config.Parameters.root((new MiniConfig).toInstance)
   it should "FPDivSqrt Node tester" in {
-    chisel3.iotesters.Driver(() => new FPDivSqrtNode(NumOuts=1,ID=1,opCode = "DIV")(t = S)) { c =>
+    chisel3.iotesters.Driver(() => new FPDivSqrtNode(NumOuts=1,ID=1,opCode = "SQRT")(t = S)) { c =>
       new FPDivNodeTester(c)
     } should be(true)
   }
