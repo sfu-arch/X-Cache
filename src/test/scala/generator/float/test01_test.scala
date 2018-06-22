33f00c2663f58770881db5889ac953427d8dd106
Amirali Sharifian <am.sharifian@gmail.com>
2018-06-21 17:00:19 -0700

    Add first example of floating point unit

---
+++ src/test/scala/generator/float/test01_test.scala
@@ -0,0 +1,157 @@
+package dataflow
+
+import chisel3._
+import chisel3.util._
+import chisel3.Module
+import chisel3.testers._
+import chisel3.iotesters._
+import org.scalatest.{FlatSpec, Matchers}
+import muxes._
+import config._
+import control._
+import util._
+import interfaces._
+import regfile._
+import memory._
+import stack._
+import arbiters._
+import loop._
+import accel._
+import node._
+import junctions._
+
+
+class testFP01MainIO(implicit val p: Parameters)  extends Module with CoreParams with CacheParams {
+  val io = IO( new CoreBundle {
+    val in = Flipped(Decoupled(new Call(List(32,32))))
+    val addr = Input(UInt(nastiXAddrBits.W))
+    val din  = Input(UInt(nastiXDataBits.W))
+    val write = Input(Bool())
+    val dout = Output(UInt(nastiXDataBits.W))
+    val out = Decoupled(new Call(List(32)))
+  })
+}
+
+class testFP01Main(implicit p: Parameters) extends testFP01MainIO {
+
+  val cache = Module(new Cache)            // Simple Nasti Cache
+  val memModel = Module(new NastiMemSlave) // Model of DRAM to connect to Cache
+  val memCopy = Mem(1024, UInt(32.W))      // Local memory just to keep track of writes to cache for validation
+
+  // Store a copy of all data written to the cache.  This is done since the cache isn't
+  // 'write through' to the memory model and we have no easy way of reading the
+  // cache contents from the testbench.
+  when(cache.io.cpu.req.valid && cache.io.cpu.req.bits.iswrite) {
+    memCopy.write((cache.io.cpu.req.bits.addr>>2).asUInt(), cache.io.cpu.req.bits.data)
+  }
+  io.dout := memCopy.read((io.addr>>2).asUInt())
+
+  // Connect the wrapper I/O to the memory model initialization interface so the
+  // test bench can write contents at start.
+  memModel.io.nasti <> cache.io.nasti
+  memModel.io.init.bits.addr := io.addr
+  memModel.io.init.bits.data := io.din
+  memModel.io.init.valid := io.write
+  cache.io.cpu.abort := false.B
+
+  // Wire up the cache and modules under test.
+  val test01_fp = Module(new testFP01DF())
+
+  cache.io.cpu.req <> test01_fp.io.MemReq
+  test01_fp.io.MemResp <> cache.io.cpu.resp
+  test01_fp.io.in <> io.in
+  io.out <> test01_fp.io.out
+
+}
+
+
+
+
+
+class testFP01Test01[T <: testFP01MainIO](c: T) extends PeekPokeTester(c) {
+
+
+  /**
+    * test02DF interface:
+    *
+    * in = Flipped(Decoupled(new Call(List(...))))
+    * out = Decoupled(new Call(List(32)))
+    */
+
+
+  // Initializing the signals
+
+  poke(c.io.in.valid, false.B)
+
+  poke(c.io.in.bits.data("field0").data, 0.U)
+  poke(c.io.in.bits.data("field0").predicate, false.B)
+  poke(c.io.in.bits.data("field1").data, 0.U)
+  poke(c.io.in.bits.data("field1").predicate, false.B)
+  poke(c.io.out.ready, false.B)
+  step(1)
+
+  poke(c.io.in.valid, true.B)
+  poke(c.io.in.bits.enable.control, true.B)
+  poke(c.io.in.bits.data("field0").data, 0x40a00000.U)
+  poke(c.io.in.bits.data("field0").predicate, true.B)
+  poke(c.io.in.bits.data("field1").data, 0x4059999a.U)
+  poke(c.io.in.bits.data("field1").predicate, true.B)
+  poke(c.io.out.ready, true.B)
+  step(1)
+
+  poke(c.io.in.valid, false.B)
+  poke(c.io.in.bits.enable.control, false.B)
+  poke(c.io.in.bits.data("field0").data, 0.U)
+  poke(c.io.in.bits.data("field0").predicate, false.B)
+  poke(c.io.in.bits.data("field1").data, 0.U)
+  poke(c.io.in.bits.data("field1").predicate, false.B)
+  poke(c.io.out.ready, true.B)
+  step(1)
+
+  var time = 1
+  var result = false
+
+  while (time < 200) {
+    time += 1
+    step(1)
+    //println(s"Cycle: $time")
+    if (peek(c.io.out.valid) == 1 &&
+      peek(c.io.out.bits.data("field0").predicate) == 1) {
+      result = true
+      val data = peek(c.io.out.bits.data("field0").data)
+      if (data != 0x41066667) {
+        println(Console.RED + s"*** Incorrect result received. Got $data. Hoping for 11")
+        fail
+      } else {
+        println(Console.BLUE + s"*** Correct result received @ cycle: $time.")
+      }
+    }
+  }
+  if (!result) {
+    println("*** Timeout.")
+    fail
+  }
+
+
+}
+
+class testFP01Tester extends FlatSpec with Matchers {
+  implicit val p = config.Parameters.root((new MiniConfig).toInstance)
+  it should "Check that testFP01 works correctly." in {
+    // iotester flags:
+    // -ll  = log level <Error|Warn|Info|Debug|Trace>
+    // -tbn = backend <firrtl|verilator|vcs>
+    // -td  = target directory
+    // -tts = seed for RNG
+    chisel3.iotesters.Driver.execute(
+      Array(
+        // "-ll", "Info",
+        "-tbn", "verilator",
+        "-td", "test_run_dir",
+        "-tts", "0001"),
+      () => new testFP01Main()) {
+      c => new testFP01Test01(c)
+    } should be(true)
+  }
+}
+
