bf97e5f06f64251aa7b6236d36907357ddad250a
Steve Margerm <smargerm@sfu.ca>
2018-08-17 16:09:09 -0700

    Added quartus template files and scripts to do test fit of designs on Altera FPGAs.

--- RTL/compile_all.sh
+++ RTL/compile_all.sh
@@ -2,34 +2,30 @@ if [ -z "${CONCURRENT}" ]; then
     CONCURRENT=2
 fi
 
-#BEGIN_TIME=`date`
+# Set family to either "Cyclone V" or "Arria 10"
+FAMILY="Cyclone V"
+PART=5CSEMA5F31C6
+
+#
+#FAMILY="Arria 10"
+#PART=10AS016E4F29E3LG
+
+USE_VCD=ON
+
 APPS=( \
-    # 'cilk_for_test04TopA' \
-    # 'cilk_for_test04TopB' \
-    # 'cilk_for_test04TopC' \
-    # 'cilk_for_test05TopA' \
-    # 'cilk_for_test05TopB' \
-    # 'cilk_for_test05TopC' \
-    # 'cilk_for_test06TopA' \
-    # 'cilk_for_test06TopB' \
-    # 'cilk_for_test06TopC' \
-    # 'cilk_for_test08TopA' \
-    # 'cilk_for_test08TopB' \
-    # 'cilk_for_test08TopC' \
-    'fibTop' \
+    'stencilTop' \
+    'dedupTop' \
 )
 
 for app in ${APPS[@]}; do
     echo "Generating Quartus project."
-    pushd ${app} &&  make_quartus_prj.sh --dir . --instance ${app} --source ${app}.v && popd
+    pushd ${app} &&  ../../scripts/make_quartus_prj.sh --dir . --instance ${app} --source ${app}.v \
+					 --vcd=${app}.vcd --use_vcd={USE_VCD} \
+					 --part ${PART} --family "${FAMILY}" && popd
 done
 
 # Use xargs to spawn a maximum of $CONCURRENT builds at a time
 printf "%s\n" ${APPS[@]} | xargs -n1 --max-procs=${CONCURRENT} -I APP bash -c 'echo "Compiling FPGA for APP"; cd APP/APP*; source build.sh >& make_compile.log'
 
 
-#END_TIME=`date`
 
-#sendEmail -f smargerm@sfu.ca -t smargerm@sfu.ca -s mailgate.sfu.ca \
-#          -u "compile_hybrids.sh complete" \
-#          -m "Begin: ${BEGIN_TIME}\nEnd  : ${END_TIME}"
---
+++ scripts/quartus_template/top.sdc
@@ -0,0 +1,2 @@
+create_clock -period 2 -name clock [get_ports clock]
+
