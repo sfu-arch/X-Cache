11bd813357b2344ebc048056bd93b06d71bb933d
Amirali Sharifian <am.sharifian@gmail.com>
2018-09-28 16:11:55 -0700

    Initializing Covariance example. The simulation works. Connections are missing but there is no run-time error

--- src/test/scala/generator/polybench/covariance_test.scala
+++ src/test/scala/generator/polybench/covariance_test.scala
@@ -20,55 +20,62 @@ import accel._
 import node._
 import junctions._
 
-class covarianceMainIO(implicit val p: Parameters)  extends Module with CoreParams with CacheParams {
-  val io = IO( new CoreBundle {
-    val in = Flipped(Decoupled(new Call(List(32))))
-    val addr = Input(UInt(nastiXAddrBits.W))
-    val din  = Input(UInt(nastiXDataBits.W))
-    val write = Input(Bool())
-    val dout = Output(UInt(nastiXDataBits.W))
-    val out = Decoupled(new Call(List(32)))
-
+class covarianceMainIO(implicit val p: Parameters) extends Module with CoreParams with CacheParams {
+  val io = IO(new CoreBundle {
+    val in = Flipped(Decoupled(new Call(List(32, 32, 32, 32))))
+    val req = Flipped(Decoupled(new MemReq))
+    val resp = Output(Valid(new MemResp))
+    val out = Decoupled(new Call(List()))
   })
 }
 
-class covarianceMainDirect(implicit p: Parameters) extends covarianceMainIO{
+class covarianceMain(implicit p: Parameters) extends covarianceMainIO {
 
-  val cache = Module(new Cache)            // Simple Nasti Cache
-  val memModel = Module(new NastiMemSlave) // Model of DRAM to connect to Cache
-  val memCopy = Mem(1024, UInt(32.W))      // Local memory just to keep track of writes to cache for validation
-
-  // Store a copy of all data written to the cache.  This is done since the cache isn't
-  // 'write through' to the memory model and we have no easy way of reading the
-  // cache contents from the testbench.
-  when(cache.io.cpu.req.valid && cache.io.cpu.req.bits.iswrite) {
-    memCopy.write((cache.io.cpu.req.bits.addr>>2).asUInt(), cache.io.cpu.req.bits.data)
-  }
-  io.dout := memCopy.read((io.addr>>2).asUInt())
+  val cache = Module(new Cache) // Simple Nasti Cache
+  val memModel = Module(new NastiCMemSlave) // Model of DRAM to connect to Cache
+  val memCopy = Mem(1024, UInt(32.W)) // Local memory just to keep track of writes to cache for validation
 
   // Connect the wrapper I/O to the memory model initialization interface so the
   // test bench can write contents at start.
   memModel.io.nasti <> cache.io.nasti
-  memModel.io.init.bits.addr := io.addr
-  memModel.io.init.bits.data := io.din
-  memModel.io.init.valid := io.write
+  memModel.io.init.bits.addr := 0.U
+  memModel.io.init.bits.data := 0.U
+  memModel.io.init.valid := false.B
   cache.io.cpu.abort := false.B
 
   // Wire up the cache and modules under test.
-  val covariance = Module(new test13DF())
+  val covariance = Module(new kernel_covarianceDF())
 
+  //Put an arbiter infront of cache
+  val CacheArbiter = Module(new MemArbiter(2))
 
-  cache.io.cpu.req <> covariance.io.MemReq
-  covariance.io.MemResp<> cache.io.cpu.resp
+  // Connect input signals to cache
+  CacheArbiter.io.cpu.MemReq(0) <> covariance.io.MemReq
+  covariance.io.MemResp <> CacheArbiter.io.cpu.MemResp(0)
 
+  //Connect main module to cache arbiter
+  CacheArbiter.io.cpu.MemReq(1) <> io.req
+  io.resp <> CacheArbiter.io.cpu.MemResp(1)
+
+  //Connect cache to the arbiter
+  cache.io.cpu.req <> CacheArbiter.io.cache.MemReq
+  CacheArbiter.io.cache.MemResp <> cache.io.cpu.resp
+
+  //Connect in/out ports
   covariance.io.in <> io.in
   io.out <> covariance.io.out
 
+  // Check if trace option is on or off
+  if (p(TRACE) == false) {
+    println(Console.RED + "****** Trace option is off. *********" + Console.RESET)
+  }
+  else
+    println(Console.BLUE + "****** Trace option is on. *********" + Console.RESET)
+
+
 }
 
-
-
-class CovarianceTest01[T <: covarianceMainDirect](c: T) extends PeekPokeTester(c) {
+class CovarianceTest01[T <: covarianceMainIO](c: T) extends PeekPokeTester(c) {
 
 
   /**
@@ -83,30 +90,40 @@ class CovarianceTest01[T <: covarianceMainDirect](c: T) extends PeekPokeTester(c
   poke(c.io.in.valid, false.B)
   poke(c.io.in.bits.data("field0").data, 0.U)
   poke(c.io.in.bits.data("field0").predicate, false.B)
-//  poke(c.io.in.bits.data("field1").data, 0.U)
-//  poke(c.io.in.bits.data("field1").predicate, false.B)
-//  poke(c.io.in.bits.data("field2").data, 0.U)
-//  poke(c.io.in.bits.data("field2").predicate, false.B)
+  poke(c.io.in.bits.data("field1").data, 0.U)
+  poke(c.io.in.bits.data("field1").predicate, false.B)
+  poke(c.io.in.bits.data("field2").data, 0.U)
+  poke(c.io.in.bits.data("field2").predicate, false.B)
+  poke(c.io.in.bits.data("field3").data, 0.U)
+  poke(c.io.in.bits.data("field3").predicate, false.B)
   poke(c.io.out.ready, false.B)
   step(1)
   poke(c.io.in.bits.enable.control, true.B)
   poke(c.io.in.valid, true.B)
-  poke(c.io.in.bits.data("field0").data, 100.U)
+
+  //Value: 1400.0f
+  poke(c.io.in.bits.data("field0").data, 1152319488.U)
   poke(c.io.in.bits.data("field0").predicate, true.B)
-//  poke(c.io.in.bits.data("field1").data, 4.U)
-//  poke(c.io.in.bits.data("field1").predicate, true.B)
-//  poke(c.io.in.bits.data("field2").data, 8.U)
-//  poke(c.io.in.bits.data("field2").predicate, true.B)
+
+  //Memory
+  poke(c.io.in.bits.data("field1").data, 196890624.U)
+  poke(c.io.in.bits.data("field1").predicate, true.B)
+  poke(c.io.in.bits.data("field2").data, 210337792.U)
+  poke(c.io.in.bits.data("field2").predicate, true.B)
+  poke(c.io.in.bits.data("field3").data, 397620428.U)
+  poke(c.io.in.bits.data("field3").predicate, true.B)
   poke(c.io.out.ready, true.B)
   step(1)
   poke(c.io.in.bits.enable.control, false.B)
   poke(c.io.in.valid, false.B)
   poke(c.io.in.bits.data("field0").data, 0.U)
   poke(c.io.in.bits.data("field0").predicate, false.B)
-//  poke(c.io.in.bits.data("field1").data, 0.U)
-//  poke(c.io.in.bits.data("field1").predicate, false.B)
-//  poke(c.io.in.bits.data("field2").data, 0.U)
-//  poke(c.io.in.bits.data("field2").predicate, false.B)
+  poke(c.io.in.bits.data("field1").data, 0.U)
+  poke(c.io.in.bits.data("field1").predicate, false.B)
+  poke(c.io.in.bits.data("field2").data, 0.U)
+  poke(c.io.in.bits.data("field2").predicate, false.B)
+  poke(c.io.in.bits.data("field3").data, 8.U)
+  poke(c.io.in.bits.data("field3").predicate, true.B)
 
   step(1)
 
@@ -115,7 +132,7 @@ class CovarianceTest01[T <: covarianceMainDirect](c: T) extends PeekPokeTester(c
   // using if() and fail command.
   var time = 1  //Cycle counter
   var result = false
-  while (time < 600) {
+  while (time < 50000) {
     time += 1
     step(1)
     //println(s"Cycle: $time")
@@ -153,7 +170,7 @@ class covarianceTester extends FlatSpec with Matchers {
        "-tbn", "verilator",
        "-td", "test_run_dir",
        "-tts", "0001"),
-     () => new covarianceMainDirect()) {
+      () => new covarianceMain()) {
      c => new CovarianceTest01(c)
     } should be(true)
   }
