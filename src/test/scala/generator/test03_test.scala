8a074b3ffbffbf75d1342d2d4b2b83f97c280e18
Amirali Sharifian <am.sharifian@gmail.com>
2019-02-10 12:55:08 -0800

    Clean up test codes

--- src/test/scala/generator/test03_test.scala
+++ src/test/scala/generator/test03_test.scala
@@ -19,52 +19,63 @@ import loop._
 import accel._
 import node._
 
-class test03MainIO(implicit val p: Parameters)  extends Module with CoreParams with CacheParams {
-  val io = IO( new CoreBundle {
-    val in = Flipped(Decoupled(new Call(List(32,32,32))))
-    val addr = Input(UInt(nastiXAddrBits.W))
-    val din  = Input(UInt(nastiXDataBits.W))
-    val write = Input(Bool())
-    val dout = Output(UInt(nastiXDataBits.W))
+class test03MainIO(implicit val p: Parameters) extends Module with CoreParams with CacheParams {
+  val io = IO(new Bundle {
+    val in = Flipped(Decoupled(new Call(List(32, 32, 32))))
+    val req = Flipped(Decoupled(new MemReq))
+    val resp = Output(Valid(new MemResp))
     val out = Decoupled(new Call(List(32)))
   })
+
+  def cloneType = new test03MainIO().asInstanceOf[this.type]
 }
 
 class test03Main(implicit p: Parameters) extends test03MainIO {
 
-  val cache = Module(new Cache)            // Simple Nasti Cache
+  val cache = Module(new Cache) // Simple Nasti Cache
   val memModel = Module(new NastiMemSlave) // Model of DRAM to connect to Cache
-  val memCopy = Mem(1024, UInt(32.W))      // Local memory just to keep track of writes to cache for validation
-
-  // Store a copy of all data written to the cache.  This is done since the cache isn't
-  // 'write through' to the memory model and we have no easy way of reading the
-  // cache contents from the testbench.
-  when(cache.io.cpu.req.valid && cache.io.cpu.req.bits.iswrite) {
-    memCopy.write((cache.io.cpu.req.bits.addr>>2).asUInt(), cache.io.cpu.req.bits.data)
-  }
-  io.dout := memCopy.read((io.addr>>2).asUInt())
+  val memCopy = Mem(1024, UInt(32.W)) // Local memory just to keep track of writes to cache for validation
 
   // Connect the wrapper I/O to the memory model initialization interface so the
   // test bench can write contents at start.
   memModel.io.nasti <> cache.io.nasti
-  memModel.io.init.bits.addr := io.addr
-  memModel.io.init.bits.data := io.din
-  memModel.io.init.valid := io.write
+  memModel.io.init.bits.addr := 0.U
+  memModel.io.init.bits.data := 0.U
+  memModel.io.init.valid := false.B
   cache.io.cpu.abort := false.B
 
   // Wire up the cache and modules under test.
   //    val test03 = Module(new test03DF())
-  val test03 = Module(new test03_optimizedDF())
+  val test03 = Module(new test03DF())
+    //Put an arbiter infront of cache
+  val CacheArbiter = Module(new MemArbiter(2))
 
-  cache.io.cpu.req <> test03.io.MemReq
-  test03.io.MemResp <> cache.io.cpu.resp
+  // Connect input signals to cache
+  CacheArbiter.io.cpu.MemReq(0) <> test03.io.MemReq
+  test03.io.MemResp <> CacheArbiter.io.cpu.MemResp(0)
+
+  //Connect main module to cache arbiter
+  CacheArbiter.io.cpu.MemReq(1) <> io.req
+  io.resp <> CacheArbiter.io.cpu.MemResp(1)
+
+  //Connect cache to the arbiter
+  cache.io.cpu.req <> CacheArbiter.io.cache.MemReq
+  CacheArbiter.io.cache.MemResp <> cache.io.cpu.resp
+
+  //Connect in/out ports
   test03.io.in <> io.in
   io.out <> test03.io.out
 
+  // Check if trace option is on or off
+  if (p(TRACE) == false) {
+    println(Console.RED + "****** Trace option is off. *********" + Console.RESET)
+  }
+  else
+    println(Console.BLUE + "****** Trace option is on. *********" + Console.RESET)
+
+
+
 }
-
-
-
 
 
 //class test03Test01(c: test03CacheWrapper) extends PeekPokeTester(c) {
@@ -72,13 +83,13 @@ class test03Test01[T <: test03MainIO](c: T) extends PeekPokeTester(c) {
 
 
   /**
-  *  test03DF interface:
-  *
-  *    data_0 = Flipped(Decoupled(new DataBundle))
-   *    val pred = Decoupled(new Bool())
-   *    val start = Input(new Bool())
-   *    val result = Decoupled(new DataBundle)
-   */
+    * test03DF interface:
+    *
+    * data_0 = Flipped(Decoupled(new DataBundle))
+    * val pred = Decoupled(new Bool())
+    * val start = Input(new Bool())
+    * val result = Decoupled(new DataBundle)
+    */
 
 
   // Initializing the signals
@@ -112,14 +123,14 @@ class test03Test01[T <: test03MainIO](c: T) extends PeekPokeTester(c) {
   poke(c.io.in.bits.data("field2").data, 0.U)
   poke(c.io.in.bits.data("field2").predicate, false.B)
   step(1)
-  var time = 1  //Cycle counter
+  var time = 1 //Cycle counter
   var result = false
   while (time < 300) {
     time += 1
     step(1)
     //println(s"Cycle: $time")
     if (peek(c.io.out.valid) == 1 &&
-        peek(c.io.out.bits.data("field0").predicate) == 1) {
+      peek(c.io.out.bits.data("field0").predicate) == 1) {
       result = true
       val data = peek(c.io.out.bits.data("field0").data)
       if (data != 3030) {
@@ -131,7 +142,7 @@ class test03Test01[T <: test03MainIO](c: T) extends PeekPokeTester(c) {
     }
   }
 
-  if(!result) {
+  if (!result) {
     println("*** Timeout.")
     fail
   }
@@ -146,13 +157,13 @@ class test03Tester extends FlatSpec with Matchers {
     // -td  = target directory
     // -tts = seed for RNG
     chisel3.iotesters.Driver.execute(
-     Array(
-       // "-ll", "Info",
-       "-tbn", "verilator",
-       "-td", "test_run_dir/test03",
-       "-tts", "0001"),
-     () => new test03Main()) {
-     c => new test03Test01(c)
+      Array(
+        // "-ll", "Info",
+        "-tbn", "verilator",
+        "-td", "test_run_dir/test03",
+        "-tts", "0001"),
+      () => new test03Main()) {
+      c => new test03Test01(c)
     } should be(true)
   }
 }
