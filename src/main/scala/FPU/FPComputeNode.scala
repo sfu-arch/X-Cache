513b4dc14f329c0e62e07632dc1c1cb79296f2d4
Amirali Sharifian <am.sharifian@gmail.com>
2019-02-22 17:24:09 -0800

    Add fp adder

--- src/main/scala/FPU/FPComputeNode.scala
+++ src/main/scala/FPU/FPComputeNode.scala
@@ -142,3 +142,151 @@ class FPComputeNode(NumOuts: Int, ID: Int, opCode: String)
   }
 
 }
+
+
+
+class altfp_adder_13(DATA: Int = 32, ADDR: Int = 32) extends BlackBox with HasBlackBoxResource {
+  val io = IO(new Bundle {
+    val clk_en = Input(Bool())
+    val clk = Input(Clock())
+    val dataa = Input(UInt(ADDR.W))
+    val datab = Input(UInt(DATA.W))
+    val result = Output(UInt(DATA.W))
+  })
+
+  setResource("/verilog/altfp_adder_13.v")
+
+}
+
+class FPAdder(DATA: Int = 32, ADDR: Int = 32) extends Module {
+  val io = IO(new Bundle {
+    val dataa = Input(UInt(ADDR.W))
+    val datab = Input(UInt(DATA.W))
+    val result = Output(UInt(DATA.W))
+  })
+
+  val fpadder = Module(new altfp_adder_13())
+
+  fpadder.io.clk := clock
+  fpadder.io.clk_en := true.B
+  fpadder.io.dataa <> io.dataa
+  fpadder.io.datab <> io.datab
+  io.result <> fpadder.io.result
+
+}
+
+
+/**
+ * [FPComputeNode description]
+ */
+class FPCustomAdderNode(NumOuts: Int, ID: Int, opCode: String)
+                 (t: FType)
+                 (implicit p: Parameters,
+                  name: sourcecode.Name,
+                  file: sourcecode.File)
+  extends HandShakingNPS(NumOuts, ID)(new DataBundle())(p) {
+  override lazy val io = IO(new FPComputeNodeIO(NumOuts))
+
+  // Printf debugging
+  val node_name = name.value
+  val module_name = file.value.split("/").tail.last.split("\\.").head.capitalize
+
+  override val printfSigil = "[" + module_name + "] " + node_name + ": " + ID + " "
+  //  override val printfSigil = "Node (COMP - " + opCode + ") ID: " + ID + " "
+  val (cycleCount, _) = Counter(true.B, 32 * 1024)
+
+  /*===========================================*
+   *            Registers                      *
+   *===========================================*/
+  // Left Input
+  val left_R = RegInit(DataBundle.default)
+  val left_valid_R = RegInit(false.B)
+
+  // Right Input
+  val right_R = RegInit(DataBundle.default)
+  val right_valid_R = RegInit(false.B)
+
+  val task_ID_R = RegNext(next = enable_R.taskID)
+
+  //Output register
+  val out_data_R = RegInit(DataBundle.default)
+
+  val s_IDLE :: s_COMPUTE :: Nil = Enum(2)
+  val state = RegInit(s_IDLE)
+
+
+  val predicate = left_R.predicate & right_R.predicate// & IsEnable()
+
+  /*===============================================*
+   *            Latch inputs. Wire up output       *
+   *===============================================*/
+
+  //Instantiate ALU with selected code. IEEE ALU. IEEE in/IEEE out
+  //  val FU = Module(new FPUALU(xlen, opCode, t))
+  val FU = Module(new FPAdder())
+  FU.io.dataa := left_R.data
+  FU.io.datab := right_R.data
+
+  io.LeftIO.ready := ~left_valid_R
+  when(io.LeftIO.fire()) {
+    left_R <> io.LeftIO.bits
+    left_valid_R := true.B
+  }
+
+  io.RightIO.ready := ~right_valid_R
+  when(io.RightIO.fire()) {
+    right_R <> io.RightIO.bits
+    right_valid_R := true.B
+  }
+
+  // Wire up Outputs
+  for (i <- 0 until NumOuts) {
+    //io.Out(i).bits.data := FU.io.out
+    //io.Out(i).bits.predicate := predicate
+    // The taskID's should be identical except in the case
+    // when one input is tied to a constant.  In that case
+    // the taskID will be zero.  Logical OR'ing the IDs
+    // Should produce a valid ID in either case regardless of
+    // which input is constant.
+    //io.Out(i).bits.taskID := left_R.taskID | right_R.taskID
+    io.Out(i).bits := out_data_R
+  }
+
+  /*============================================*
+   *            State Machine                   *
+   *============================================*/
+  switch(state) {
+    is(s_IDLE) {
+      when(enable_valid_R) {
+        when(left_valid_R && right_valid_R) {
+          ValidOut()
+          when(enable_R.control) {
+            out_data_R.data := FU.io.result
+            out_data_R.predicate := predicate
+            out_data_R.taskID := left_R.taskID | right_R.taskID | enable_R.taskID
+          }
+          state := s_COMPUTE
+        }
+      }
+    }
+    is(s_COMPUTE) {
+      when(IsOutReady()) {
+        // Reset data
+        //left_R := DataBundle.default
+        //right_R := DataBundle.default
+        left_valid_R := false.B
+        right_valid_R := false.B
+        //Reset state
+        state := s_IDLE
+        //Reset output
+        out_data_R.predicate := false.B
+        Reset()
+        if (log) {
+          printf("[LOG] " + "[" + module_name + "] " + "[TID->%d] "
+            + node_name + ": Output fired @ %d, Value: %x\n", task_ID_R, cycleCount, FU.io.result)
+        }
+      }
+    }
+  }
+
+}
