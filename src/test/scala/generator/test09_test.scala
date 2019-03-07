d29ff8d03b143921b3334244742fbabc52465ce2
Amirali Sharifian <am.sharifian@gmail.com>
2019-03-07 15:05:23 -0800

    Update test09

--- src/test/scala/generator/test09_test.scala
+++ src/test/scala/generator/test09_test.scala
@@ -1,5 +1,7 @@
 package dataflow
 
+import java.io.PrintWriter
+import java.io.File
 import chisel3._
 import chisel3.util._
 import chisel3.Module
@@ -19,127 +21,199 @@ import loop._
 import accel._
 import node._
 
-//class test09CacheWrapper()(implicit p: Parameters) extends test09DF()(p)
-  //with CacheParams {
 
-  //// Instantiate the AXI Cache
-  //val cache = Module(new Cache)
-  //cache.io.cpu.req <> CacheMem.io.MemReq
-  //CacheMem.io.MemResp <> cache.io.cpu.resp
-  //cache.io.cpu.abort := false.B
-  //// Instantiate a memory model with AXI slave interface for cache
-  //val memModel = Module(new NastiMemSlave)
-  //memModel.io.nasti <> cache.io.nasti
-
-//}
-
-class test09MainIO(implicit val p: Parameters)  extends Module with CoreParams with CacheParams {
-  val io = IO( new CoreBundle {
-    val in = Flipped(Decoupled(new Call(List(32,32,32))))
-    val addr = Input(UInt(nastiXAddrBits.W))
-    val din  = Input(UInt(nastiXDataBits.W))
-    val write = Input(Bool())
-    val dout = Output(UInt(nastiXDataBits.W))
+class test09MainIO(implicit val p: Parameters) extends Module with CoreParams with CacheParams {
+  val io = IO(new Bundle {
+    val in = Flipped(Decoupled(new Call(List(32, 32, 32))))
+    val req = Flipped(Decoupled(new MemReq))
+    val resp = Output(Valid(new MemResp))
     val out = Decoupled(new Call(List(32)))
   })
+
+  def cloneType = new test09MainIO().asInstanceOf[this.type]
 }
 
 class test09Main(implicit p: Parameters) extends test09MainIO {
 
-  val cache = Module(new Cache)            // Simple Nasti Cache
+  val cache = Module(new Cache) // Simple Nasti Cache
   val memModel = Module(new NastiMemSlave) // Model of DRAM to connect to Cache
-  val memCopy = Mem(1024, UInt(32.W))      // Local memory just to keep track of writes to cache for validation
+  val memCopy = Mem(1024, UInt(32.W)) // Local memory just to keep track of writes to cache for validation
 
-  // Store a copy of all data written to the cache.  This is done since the cache isn't
-  // 'write through' to the memory model and we have no easy way of reading the
-  // cache contents from the testbench.
-  when(cache.io.cpu.req.valid && cache.io.cpu.req.bits.iswrite) {
-    memCopy.write((cache.io.cpu.req.bits.addr>>2).asUInt(), cache.io.cpu.req.bits.data)
-  }
-  io.dout := memCopy.read((io.addr>>2).asUInt())
 
   // Connect the wrapper I/O to the memory model initialization interface so the
   // test bench can write contents at start.
   memModel.io.nasti <> cache.io.nasti
-  memModel.io.init.bits.addr := io.addr
-  memModel.io.init.bits.data := io.din
-  memModel.io.init.valid := io.write
+  memModel.io.init.bits.addr := 0.U
+  memModel.io.init.bits.data := 0.U
+  memModel.io.init.valid := true.B
   cache.io.cpu.abort := false.B
 
   // Wire up the cache and modules under test.
   val test09 = Module(new test09DF())
 
-  cache.io.cpu.req <> test09.io.MemReq
-  test09.io.MemResp <> cache.io.cpu.resp
+  //Put an arbiter infront of cache
+  val CacheArbiter = Module(new MemArbiter(2))
+
+  // Connect input signals to cache
+  CacheArbiter.io.cpu.MemReq(0) <> test09.io.MemReq
+  test09.io.MemResp <> CacheArbiter.io.cpu.MemResp(0)
+
+  //Connect main module to cache arbiter
+  CacheArbiter.io.cpu.MemReq(1) <> io.req
+  io.resp <> CacheArbiter.io.cpu.MemResp(1)
+
+  //Connect cache to the arbiter
+  cache.io.cpu.req <> CacheArbiter.io.cache.MemReq
+  CacheArbiter.io.cache.MemResp <> cache.io.cpu.resp
+
+  //Connect in/out ports
   test09.io.in <> io.in
   io.out <> test09.io.out
+
 
 }
 
 
-
-
-//class test09Test01(c: test09CacheWrapper) extends PeekPokeTester(c) {
 class test09Test01[T <: test09MainIO](c: T) extends PeekPokeTester(c) {
 
+  val inAddrVecA = List.range(0, 4 * 10, 4)
+  val inDataVecA = List(1, 2, 3, 4, 5, 1, 2, 4, 5)
 
-  /**
-  *  test09DF interface:
-  *
-  *    data_0 = Flipped(Decoupled(new DataBundle))
-   *    val pred = Decoupled(new Bool())
-   *    val start = Input(new Bool())
-   *    val result = Decoupled(new DataBundle)
-   */
+  val outAddrVec = List.range(40, 4 * 5, 4)
+  val outDataVec = List.range(1, 6, 1).map(_ * 2)
+
+  def MemRead(addr: Int): BigInt = {
+    while (peek(c.io.req.ready) == 0) {
+      step(1)
+    }
+    poke(c.io.req.valid, 1)
+    poke(c.io.req.bits.addr, addr)
+    poke(c.io.req.bits.iswrite, 0)
+    poke(c.io.req.bits.tag, 0)
+    poke(c.io.req.bits.mask, 0)
+    poke(c.io.req.bits.mask, -1)
+    step(1)
+    while (peek(c.io.resp.valid) == 0) {
+      step(1)
+    }
+    val result = peek(c.io.resp.bits.data)
+    result
+  }
+
+  def MemWrite(addr: Int, data: Int): BigInt = {
+    while (peek(c.io.req.ready) == 0) {
+      step(1)
+    }
+    poke(c.io.req.valid, 1)
+    poke(c.io.req.bits.addr, addr)
+    poke(c.io.req.bits.data, data)
+    poke(c.io.req.bits.iswrite, 1)
+    poke(c.io.req.bits.tag, 0)
+    poke(c.io.req.bits.mask, 0)
+    poke(c.io.req.bits.mask, -1)
+    step(1)
+    poke(c.io.req.valid, 0)
+    1
+  }
+
+  def dumpMemory(path: String) = {
+    //Writing mem states back to the file
+    val pw = new PrintWriter(new File(path))
+    for (i <- 0 until outDataVec.length) {
+      val data = MemRead(outAddrVec(i))
+      pw.write("0X" + outAddrVec(i).toHexString + " -> " + data + "\n")
+    }
+    pw.close
+
+  }
+
+
+  for (i <- 0 until inDataVecA.length) {
+    MemWrite(inAddrVecA(i), inDataVecA(i))
+  }
 
 
   // Initializing the signals
-
-  poke(c.io.in.valid, false.B)
   poke(c.io.in.bits.enable.control, false.B)
+  poke(c.io.in.valid, false.B)
   poke(c.io.in.bits.data("field0").data, 0.U)
+  poke(c.io.in.bits.data("field0").taskID, 5.U)
   poke(c.io.in.bits.data("field0").predicate, false.B)
-  poke(c.io.out.ready, true.B)
+  poke(c.io.in.bits.data("field1").data, 0.U)
+  poke(c.io.in.bits.data("field1").taskID, 5.U)
+  poke(c.io.in.bits.data("field1").predicate, false.B)
+  poke(c.io.in.bits.data("field2").data, 0.U)
+  poke(c.io.in.bits.data("field2").taskID, 5.U)
+  poke(c.io.in.bits.data("field2").predicate, false.B)
+  poke(c.io.out.ready, false.B)
   step(1)
-  poke(c.io.in.valid, true.B)
   poke(c.io.in.bits.enable.control, true.B)
-  poke(c.io.in.bits.data("field0").data, 100.U)
+  poke(c.io.in.valid, true.B)
+  poke(c.io.in.bits.data("field0").data, 0) // Array a[] base address
   poke(c.io.in.bits.data("field0").predicate, true.B)
+  poke(c.io.in.bits.data("field1").data, 20) // Array b[]
+  poke(c.io.in.bits.data("field1").predicate, true.B)
+  poke(c.io.in.bits.data("field2").data, 40) // Array c[]
+  poke(c.io.in.bits.data("field2").predicate, true.B)
   poke(c.io.out.ready, true.B)
   step(1)
-  poke(c.io.in.valid, false.B)
   poke(c.io.in.bits.enable.control, false.B)
-  poke(c.io.in.bits.data("field0").data, 0.U)
+  poke(c.io.in.valid, false.B)
+  poke(c.io.in.bits.data("field0").data, 0)
   poke(c.io.in.bits.data("field0").predicate, false.B)
+  poke(c.io.in.bits.data("field1").data, 0.U)
+  poke(c.io.in.bits.data("field1").predicate, false.B)
+  poke(c.io.in.bits.data("field2").data, 0.U)
+  poke(c.io.in.bits.data("field2").predicate, false.B)
 
   step(1)
-  var time = 1  //Cycle counter
+
+  // NOTE: Don't use assert().  It seems to terminate the writing of VCD files
+  // early (before the error) which makes debugging very difficult. Check results
+  // using if() and fail command.
+  var time = 0
   var result = false
-  while (time < 200) {
+  while (time < 9000) {
     time += 1
     step(1)
-    //println(s"Cycle: $time")
     if (peek(c.io.out.valid) == 1 &&
-      peek(c.io.out.bits.data("field0").predicate) == 1){
+      peek(c.io.out.bits.data("field0").predicate) == 1
+    ) {
       result = true
       val data = peek(c.io.out.bits.data("field0").data)
-      if (data != 5) {
-        println(Console.RED + s"*** Incorrect result received. Got $data. Hoping for 5")
+      if (data != 1) {
+        println(Console.RED + s"*** Incorrect result received. Got $data. Hoping for 1" + Console.RESET)
         fail
       } else {
-        println(Console.BLUE + s"*** Correct result received.")
+        println(Console.BLUE + s"*** Correct return result received. Run time: $time cycles." + Console.RESET)
       }
     }
   }
 
-  if(!result) {
-    println("*** Timeout.")
-    fail
+  step(200)
+
+  //  Peek into the CopyMem to see if the expected data is written back to the Cache
+  var valid_data = true
+  for (i <- 0 until outAddrVec.length) {
+    val data = MemRead(outAddrVec(i))
+    if (data != outDataVec(i).toInt) {
+      println(Console.RED + s"*** Incorrect data received addr=${outAddrVec(i)}. Got $data. Hoping for ${outDataVec(i).toInt}" + Console.RESET)
+      fail
+      valid_data = false
+    }
+  }
+  if (valid_data) {
+    println(Console.BLUE + "*** Correct data written back." + Console.RESET)
   }
 
+
+  if (!result) {
+    println(Console.RED + "*** Timeout." + Console.RESET)
+    fail
+  }
 }
 
-class test09Tester extends FlatSpec with Matchers {
+class test09Tester1 extends FlatSpec with Matchers {
   implicit val p = config.Parameters.root((new MiniConfig).toInstance)
   it should "Check that test09 works correctly." in {
     // iotester flags:
@@ -148,14 +222,13 @@ class test09Tester extends FlatSpec with Matchers {
     // -td  = target directory
     // -tts = seed for RNG
     chisel3.iotesters.Driver.execute(
-     Array(
-       // "-ll", "Info",
-       "-tbn", "verilator",
-       "-td", "test_run_dir",
-       "-tts", "0001"),
-     () => new test09Main()) {
-     c => new test09Test01(c)
+      Array(
+        // "-ll", "Info",
+        "-tbn", "verilator",
+        "-td", "test_run_dir/test09",
+        "-tts", "0001"),
+      () => new test09Main()) {
+      c => new test09Test01(c)
     } should be(true)
   }
 }
-
