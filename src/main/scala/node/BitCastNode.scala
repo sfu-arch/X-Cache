bc7d5831a5a8c4fa6f81e0dea38a0103006f71c2
Amirali Sharifian <am.sharifian@gmail.com>
2019-07-18 23:13:35 -0700

    Clean up the nodes

--- src/main/scala/node/BitCastNode.scala
+++ src/main/scala/node/BitCastNode.scala
@@ -3,37 +3,30 @@ package dandelion.node
 import chisel3._
 import chisel3.Module
 import dandelion.config._
-import dandelion.interfaces.{ControlBundle, DataBundle}
+import dandelion.interfaces._
 import util._
 import utility.UniformPrintfs
 
-class BitCastNodeIO(val src: Int, val des: Int, val nout: Int)
-                (implicit p: Parameters) extends CoreBundle()(p) {
 
-  //Input for Zext
+class BitCastNodeIO(NumOuts: Int)
+                (implicit p: Parameters)
+  extends HandShakingIONPS(NumOuts)(new DataBundle) {
+  // LeftIO: Left input data for computation
   val Input = Flipped(Decoupled(new DataBundle()))
-  //val Input = Flipped(Decoupled(UInt(src.W)))
 
-  //Enabl signal
-  val enable = Flipped(Decoupled(new ControlBundle()))
 
-  //Output of the input (Zexted version)
-  val Out = Vec(nout, Decoupled(new DataBundle()))
-  //val Out = Output(Vec(nout, Decoupled(UInt(des.W))))
-
-  override def cloneType = new ZextNodeIO(src, des, nout).asInstanceOf[this.type]
+  override def cloneType = new BitCastNodeIO(NumOuts).asInstanceOf[this.type]
 
 }
 
-class BitCastNode(val SrcW: Int = 0, val DesW: Int = 0, val NumOuts: Int = 1, val ID: Int = 0)
-              (implicit val p: Parameters,
+class BitCastNode(NumOuts: Int, ID: Int)
+              (implicit p: Parameters,
                name: sourcecode.Name,
                file: sourcecode.File)
-  extends Module with CoreParams with UniformPrintfs {
+  extends HandShakingNPS(NumOuts, ID)(new DataBundle())(p) {
+  override lazy val io = IO(new BitCastNodeIO(NumOuts))
 
-  lazy val io = IO(new ZextNodeIO(SrcW, DesW, NumOuts))
-
-  // Printf debugging
+   // Printf debugging
   val node_name = name.value
   val module_name = file.value.split("/").tail.last.split("\\.").head.capitalize
 
@@ -43,111 +36,70 @@ class BitCastNode(val SrcW: Int = 0, val DesW: Int = 0, val NumOuts: Int = 1, va
   /*===========================================*
    *            Registers                      *
    *===========================================*/
-  // Right Input
-  val input_R = RegInit(DataBundle.default)
-  val input_valid_R = RegInit(false.B)
+  // Left Input
+  val data_R = RegInit(DataBundle.default)
+  val data_valid_R = RegInit(false.B)
 
-  val enable_R = RegInit(ControlBundle.default)
-  val enable_valid_R = RegInit(false.B)
+  val s_IDLE :: s_COMPUTE :: Nil = Enum(2)
+  val state = RegInit(s_IDLE)
 
-  val output_valid_R = Seq.fill(NumOuts)(RegInit(false.B))
 
-  val fire_R = Seq.fill(NumOuts)(RegInit(false.B))
-
-  val task_input = (io.enable.bits.taskID | enable_R.taskID)
+  //Output register
+  val out_data_R = RegNext(Mux(enable_R.control, data_R.data, 0.U), init = 0.U)
+  val predicate = Mux(enable_valid_R, enable_R.control ,io.enable.bits.control)
+  val taskID = Mux(enable_valid_R, enable_R.taskID ,io.enable.bits.taskID)
 
   /*===============================================*
    *            Latch inputs. Wire up output       *
    *===============================================*/
 
-
-  io.Input.ready := ~input_valid_R
+  io.Input.ready := ~data_valid_R
   when(io.Input.fire()) {
-    input_R <> io.Input.bits
-    input_valid_R := true.B
+    data_R <> io.Input.bits
+    data_valid_R := true.B
   }
 
-  io.enable.ready := ~enable_valid_R
-  when(io.enable.fire()) {
-    enable_R <> io.enable.bits
-    enable_valid_R := true.B
-  }
 
-  // Defalut values for output
-
-  for (i <- 0 until NumOuts) {
-    io.Out(i).bits <> input_R
-    io.Out(i).valid <> output_valid_R(i)
-  }
-
-  for (i <- 0 until NumOuts) {
-    when(io.Out(i).fire) {
-      output_valid_R(i) := false.B
-      fire_R(i) := true.B
-    }
-  }
-
-  val fire_mask = (fire_R zip io.Out.map(_.fire)).map { case (a, b) => a | b }
-
-  def IsEnableValid(): Bool = {
-    return enable_valid_R || io.enable.fire
-  }
-
-  def IsInputValid(): Bool = {
-    return input_valid_R || io.Input.fire
-  }
-
-  def IsfireValid(): Bool = {
-    if(NumOuts == 0){
-      true.B
-    }
-    else{
-      fire_mask.reduce(_ & _)
-    }
-  }
-
+  // Wire up Outputs
+  // The taskID's should be identical except in the case
+  // when one input is tied to a constant.  In that case
+  // the taskID will be zero.  Logical OR'ing the IDs
+  // Should produce a valid ID in either case regardless of
+  // which input is constant.
+  io.Out.foreach(_.bits := DataBundle(out_data_R, taskID, predicate))
 
   /*============================================*
-   *            ACTIONS (possibly dangerous)    *
+   *            State Machine                   *
    *============================================*/
-  val s_idle :: s_fire :: Nil = Enum(2)
-  val state = RegInit(s_idle)
-
   switch(state) {
-    is(s_idle) {
-
-      when(IsEnableValid() && IsInputValid()) {
-
-        output_valid_R.foreach(_ := true.B)
-
-        state := s_fire
-
+    is(s_IDLE) {
+      when(enable_valid_R && data_valid_R) {
+        io.Out.foreach(_.bits := DataBundle(data_R.data, taskID, predicate))
+        io.Out.foreach(_.valid := true.B)
+        ValidOut()
+        state := s_COMPUTE
         if (log) {
-          printf(f"[LOG] " + "[" + module_name + "] " + "[TID->%d] "
-            + node_name + ": Output fired @ %d, Value: %d\n",
-            task_input, cycleCount, input_R.data)
+          printf("[LOG] " + "[" + module_name + "] " + "[TID->%d] [CMP] " +
+            node_name + ": Output fired @ %d, Value: %d\n", taskID, cycleCount, data_R.data)
         }
       }
     }
+    is(s_COMPUTE) {
+      when(IsOutReady()) {
+        // Reset data
+        data_valid_R := false.B
 
-    is(s_fire) {
-      when(IsfireValid()) {
+        out_data_R := 0.U
 
-        input_R := DataBundle.default
-        input_valid_R := false.B
+        //Reset state
+        state := s_IDLE
+        Reset()
 
-        enable_R := ControlBundle.default
-        enable_valid_R := false.B
 
-        output_valid_R.foreach(_ := false.B)
-
-        fire_R.foreach(_ := false.B)
-
-        state := s_idle
       }
     }
   }
 
-
 }
+
 
