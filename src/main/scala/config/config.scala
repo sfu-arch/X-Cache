684ca77aca34f132c239f26ab37bf3c4a045c34c
Amirali Sharifian <am.sharifian@gmail.com>
2019-09-30 11:47:47 -0700

    Cleaning the config file

--- src/main/scala/config/config.scala
+++ src/main/scala/config/config.scala
@@ -31,57 +31,39 @@ case object FTYP extends Field[FType]
 
 abstract trait CoreParams {
   implicit val p: Parameters
-  val xlen    = p(XLEN)
-  val tlen    = p(TLEN)
-  val glen    = p(GLEN)
-  val Typ_SZ  = p(TYPSZ)
-  val Beats   = Typ_SZ / xlen
+  val xlen = p(XLEN)
+  val tlen = p(TLEN)
+  val glen = p(GLEN)
+  val Typ_SZ = p(TYPSZ)
+  val Beats = Typ_SZ / xlen
   val mshrlen = p(MSHRLEN)
-  val Ftyp    = p(FTYP)
+  val Ftyp = p(FTYP)
   // Debugging dumps
-  val log      = p(TRACE)
-  val clog     = p(CTRACE)
-  val verb    = p(VERBOSITY)
-  val comp    = p(COMPONENTS)
+  val log = p(TRACE)
+  val clog = p(CTRACE)
+  val verb = p(VERBOSITY)
+  val comp = p(COMPONENTS)
 
 }
 
-abstract class CoreBundle(implicit val p: Parameters) extends ParameterizedBundle( )(p) with CoreParams
+abstract class CoreBundle(implicit val p: Parameters) extends ParameterizedBundle()(p) with CoreParams
 
 
 class MiniConfig extends Config((site, here, up) => {
-    // Core
-    case XLEN       => 32
-    case TLEN       => 5
-    case GLEN       => 16
-    // # Max bits of cache request tag.
-    case MSHRLEN  => 8
-    case TYPSZ      => 64
-    case VERBOSITY  => "low"
-    //    case COMPONENTS => "TYPLOAD;TYPOP;TYPSTORE"
-    case COMPONENTS => ""
-    // Max size of type memory system may see
-    case TRACE => true
-    case CTRACE => false
-    case BuildRFile => (p: Parameters) => Module(new RFile(32)(p))
-
-    //-------------------------
-    // Cache
-    case NWays => 1 // TODO: set-associative
-    case NSets => 256
-    case CacheBlockBytes => 4 * (here(XLEN) >> 3) // 4 x 32 bits = 16B
-    // NastiIO
-    case NastiKey => new NastiParameters(
-      idBits   = 12,
-      dataBits = 32,
-      addrBits = 32)
-
-       case FTYP => site(XLEN) match {
-        case 32 => FType.S
-        case 64 => FType.D
-        case 16 => FType.H
-        case _ => FType.S
-    }
+  // Core
+  case XLEN => 64
+  case TLEN => 5
+  case GLEN => 16
+  // # Max bits of cache request tag.
+  case MSHRLEN => 8
+  case TYPSZ => 64
+  case VERBOSITY => "low"
+  //    case COMPONENTS => "TYPLOAD;TYPOP;TYPSTORE"
+  case COMPONENTS => ""
+  // Max size of type memory system may see
+  case TRACE => true
+  case CTRACE => false
+  case BuildRFile => (p: Parameters) => Module(new RFile(32)(p))
 
   //-------------------------
   // Cache
@@ -91,7 +73,7 @@ class MiniConfig extends Config((site, here, up) => {
   // NastiIO
   case NastiKey => new NastiParameters(
     idBits = 12,
-    dataBits = 32,
+    dataBits = 64,
     addrBits = 32)
 
   case FTYP => site(XLEN) match {
@@ -100,7 +82,6 @@ class MiniConfig extends Config((site, here, up) => {
     case 16 => FType.H
     case _ => FType.S
   }
-
 }
 
 )
