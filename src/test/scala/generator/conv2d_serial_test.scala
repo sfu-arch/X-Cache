be013508a4752c0e18baff9b84e3f54ce197da2d
Amirali Sharifian <am.sharifian@gmail.com>
2019-11-21 10:43:05 -0800

    Update conv2d testcase

--- src/test/scala/generator/conv2d_serial_test.scala
+++ src/test/scala/generator/conv2d_serial_test.scala
@@ -4,7 +4,7 @@ package dandelion.generator
 import chisel3._
 import chisel3.Module
 import org.scalatest.{FlatSpec, Matchers}
-import dandelion.concurrent.{TaskController,TaskControllerIO}
+import dandelion.concurrent.{TaskController, TaskControllerIO}
 import dandelion.config._
 import dandelion.memory._
 import dandelion.accel._
@@ -12,7 +12,7 @@ import dandelion.interfaces.NastiMemSlave
 import helpers._
 
 
-class conv2DSerialSerialMainDirect(NumTiles: Int = 1)(implicit p: Parameters)
+class conv2DSerialMainDirect()(implicit p: Parameters)
   extends AccelIO(List(32, 32, 32, 32), List(32))(p) {
 
   val cache = Module(new Cache) // Simple Nasti Cache
@@ -27,7 +27,7 @@ class conv2DSerialSerialMainDirect(NumTiles: Int = 1)(implicit p: Parameters)
   cache.io.cpu.abort := false.B
 
 
-  val testDF = Module(new conv2DSerialSerialDF())
+  val testDF = Module(new conv2dSerialDF())
 
 
   val CacheArb = Module(new MemArbiter(2))
@@ -49,46 +49,76 @@ class conv2DSerialSerialMainDirect(NumTiles: Int = 1)(implicit p: Parameters)
 
 }
 
-class conv2DSerialSerialTest01[T <: AccelIO](c: T, tiles: Int)
+class conv2DSerialTest01[T <: AccelIO](c: T)
                                       (inAddrVec: List[Int], inDataVec: List[Int],
-                                       outAddrVec: List[Int], outDataVec: List[Int])
-  extends AccelTesterLocal(c)(inAddrVec, inDataVec, outAddrVec, outDataVec) {
+                                       outAddrVec: List[Int], outDataVec: List[Int],
+                                       coeffsAddrVec: List[Int], coeffsDataVec: List[Int],
+                                       inAddrBegin: Int, outAddrBegin: Int, coeffsAddrBegin: Int
+                                      )
+  extends AccelTesterLocal(c)(inAddrVec, inDataVec, outAddrVec ++ coeffsAddrVec, outDataVec ++ coeffsDataVec) {
 
   initMemory()
 
   // Initializing the signals
   poke(c.io.in.bits.enable.control, false)
-  poke(c.io.in.bits.enable.taskID, 0)
+  poke(c.io.in.bits.enable.taskID, 0.U)
   poke(c.io.in.valid, false)
-  poke(c.io.in.bits.data("field0").data, 0)
-  poke(c.io.in.bits.data("field0").taskID, 0)
+  poke(c.io.in.bits.data("field0").data, 0.U)
+  poke(c.io.in.bits.data("field0").taskID, 0.U)
   poke(c.io.in.bits.data("field0").predicate, false)
-  poke(c.io.in.bits.data("field1").data, 0)
-  poke(c.io.in.bits.data("field1").taskID, 0)
+  poke(c.io.in.bits.data("field1").data, 0.U)
+  poke(c.io.in.bits.data("field1").taskID, 0.U)
   poke(c.io.in.bits.data("field1").predicate, false)
-  poke(c.io.in.bits.data("field2").data, 0)
-  poke(c.io.in.bits.data("field2").taskID, 0)
+  poke(c.io.in.bits.data("field2").data, 0.U)
+  poke(c.io.in.bits.data("field2").taskID, 0.U)
   poke(c.io.in.bits.data("field2").predicate, false)
-  poke(c.io.in.bits.data("field3").data, 0)
-  poke(c.io.in.bits.data("field3").taskID, 0)
+
+  poke(c.io.in.bits.data("field3").data, 0.U)
+  poke(c.io.in.bits.data("field3").taskID, 0.U)
   poke(c.io.in.bits.data("field3").predicate, false)
+
+  poke(c.io.in.bits.data("field4").data, 0.U)
+  poke(c.io.in.bits.data("field4").taskID, 0.U)
+  poke(c.io.in.bits.data("field4").predicate, false)
+
+  poke(c.io.in.bits.data("field5").data, 0.U)
+  poke(c.io.in.bits.data("field5").taskID, 0.U)
+  poke(c.io.in.bits.data("field5").predicate, false)
+
+  poke(c.io.in.bits.data("field6").data, 0.U)
+  poke(c.io.in.bits.data("field6").taskID, 0.U)
+  poke(c.io.in.bits.data("field6").predicate, false)
+
   poke(c.io.out.ready, false)
   step(1)
   poke(c.io.in.bits.enable.control, true)
   poke(c.io.in.valid, true)
-  poke(c.io.in.bits.data("field0").data, 0) // Array a[] base address
+  poke(c.io.in.bits.data("field0").data, inAddrBegin.U) // Array a[] base address
   poke(c.io.in.bits.data("field0").predicate, true)
-  poke(c.io.in.bits.data("field1").data, 1024) // Array b[] base address
+  poke(c.io.in.bits.data("field1").data, outAddrBegin.U) // Array out[] base address
   poke(c.io.in.bits.data("field1").predicate, true)
-  poke(c.io.in.bits.data("field2").data, 800) // scale value
+  poke(c.io.in.bits.data("field2").data, coeffsAddrBegin.U) // Array out[] base address
   poke(c.io.in.bits.data("field2").predicate, true)
-  poke(c.io.in.bits.data("field3").data, 100)
-  poke(c.io.in.bits.data("field3").predicate, false)
+
+  poke(c.io.in.bits.data("field3").data, 10.U) // scale value
+  poke(c.io.in.bits.data("field3").predicate, true)
+
+  poke(c.io.in.bits.data("field4").data, 10.U)
+  poke(c.io.in.bits.data("field4").predicate, false)
+
+  poke(c.io.in.bits.data("field5").data, 5.U)
+  poke(c.io.in.bits.data("field5").predicate, false)
+
+  poke(c.io.in.bits.data("field6").data, 8.U)
+  poke(c.io.in.bits.data("field6").predicate, false)
+
   poke(c.io.out.ready, true.B)
+
   step(1)
   poke(c.io.in.bits.enable.control, false.B)
   poke(c.io.in.valid, false.B)
-  poke(c.io.in.bits.data("field0").data, 0)
+
+  poke(c.io.in.bits.data("field0").data, 0.U)
   poke(c.io.in.bits.data("field0").predicate, false)
   poke(c.io.in.bits.data("field1").data, 0.U)
   poke(c.io.in.bits.data("field1").predicate, false)
@@ -96,6 +126,12 @@ class conv2DSerialSerialTest01[T <: AccelIO](c: T, tiles: Int)
   poke(c.io.in.bits.data("field2").predicate, false)
   poke(c.io.in.bits.data("field3").data, 0.U)
   poke(c.io.in.bits.data("field3").predicate, false)
+  poke(c.io.in.bits.data("field4").data, 0.U)
+  poke(c.io.in.bits.data("field4").predicate, false)
+  poke(c.io.in.bits.data("field5").data, 0.U)
+  poke(c.io.in.bits.data("field5").predicate, false)
+  poke(c.io.in.bits.data("field6").data, 0.U)
+  poke(c.io.in.bits.data("field6").predicate, false)
 
   step(1)
 
@@ -115,7 +151,7 @@ class conv2DSerialSerialTest01[T <: AccelIO](c: T, tiles: Int)
         println(Console.RED + s"*** Incorrect result received. Got $data. Hoping for 1" + Console.RESET)
         fail
       } else {
-        println(Console.BLUE + s"*** Return received for t=$tiles. Run time: $time cycles." + Console.RESET)
+        println(Console.BLUE + s"*** Return received.  Run time: $time cycles." + Console.RESET)
       }
     }
   }
@@ -132,19 +168,27 @@ class conv2DSerialSerialTest01[T <: AccelIO](c: T, tiles: Int)
 
 class conv2DSerialSerialTester1 extends FlatSpec with Matchers {
 
-  val inDataVec = List(0, 0, 9, 221, 178, 152, 80, 98, 163, 40, 239, 169, 89, 179, 98, 69, 117, 196, 16, 44, 12, 59,
-    23, 14, 68, 13, 57, 137, 175, 195, 165, 68, 199, 71, 34, 0, 249, 139, 118, 157, 76, 254, 71, 190, 179, 66, 157, 193, 7,
-    198, 0, 44, 2, 182, 236, 247, 221, 190, 129, 141, 131, 192, 106, 227, 8, 166, 246, 154, 202, 0, 56, 24, 0, 25,
-    111, 57, 0, 13, 0, 21, 108, 154, 242, 7, 82, 95, 0, 200, 31, 26, 238, 59, 115, 89, 183, 21, 152, 174, 200, 100)
-  val inAddrVec = List.range(0, 4 * inDataVec.length, 4)
+  val IMG_SIZE = 10
+  val COE_SIZE = IMG_SIZE/2
+  val BYTE_SIZE = 4
 
-  val outAddrVec = List.range(1024, 1024 + (4 * inDataVec.length), 4)
-  val outDataVec = List(0, 0, 28, 255, 255, 255, 250, 255, 255, 125, 255, 255, 255, 255, 255, 215, 255, 255, 50,
-    137, 37, 184, 71, 43, 212, 40, 178, 255, 255, 255, 255, 212, 255, 221, 106, 0, 255, 255, 255, 255, 237, 255, 221,
-    255, 255, 206, 255, 255, 21, 255, 0, 137, 6, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 25, 255, 255,
-    255, 255, 0, 175, 75, 0, 78, 255, 178, 0, 40, 0, 65, 255, 255, 255, 21, 255, 255, 0, 255, 96, 81, 255, 184, 255, 255,
-    255, 65, 255, 255, 255, 255)
+  val inAddrBegin = 0
+  val inDataVec = List.range(inAddrBegin, IMG_SIZE * IMG_SIZE)
+  val inAddrVec = List.range(inAddrBegin, BYTE_SIZE * inDataVec.length, BYTE_SIZE)
 
+  val outDataVec = List(
+    0, 0, 0,  0,  0,  0,  0,  0,  0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0, 0,
+    0, 0, 22, 23, 24, 25, 26, 27, 0, 0, 0, 0, 32, 33, 34, 35, 36, 37, 0, 0,
+    0, 0, 42, 43, 44, 45, 46, 47, 0, 0, 0, 0, 52, 53, 54, 55, 56, 57, 0, 0,
+    0, 0, 62, 63, 64, 65, 66, 67, 0, 0, 0, 0, 72, 73, 74, 75, 76, 77, 0, 0,
+    0, 0, 0,  0,  0,  0,  0,  0,  0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0, 0)
+  val outAddrBegin = BYTE_SIZE * (IMG_SIZE * IMG_SIZE)
+  val outAddrVec = List.range(outAddrVec, outAddrBegin + (BYTE_SIZE * outDataVec.length), BYTE_SIZE)
+
+  val coeffsData = List(1, 4, 6, 4, 1, 4, 16, 24, 16, 4, 6,
+    24, 36, 24, 6, 4, 16, 24, 16, 4, 1, 4, 6, 4, 1)
+  val coeffsAddrBegin = 2 * BYTE_SIZE * (IMG_SIZE * IMG_SIZE)
+  val coeffsAddr = List.range(coeffsAddrBegin, coeffsAddrBegin + (BYTE_SIZE * coeffsData.length), BYTE_SIZE)
 
   implicit val p = Parameters.root((new MiniConfig).toInstance)
   val testParams = p.alterPartial({
@@ -164,8 +208,9 @@ class conv2DSerialSerialTester1 extends FlatSpec with Matchers {
         "-tbn", "verilator",
         "-td", s"test_run_dir/conv2DSerialSerial_direct",
         "-tts", "0001"),
-      () => new conv2DSerialSerialMainDirect(1)(testParams)) {
-      c => new conv2DSerialSerialTest01(c, 1)(inAddrVec, inDataVec, outAddrVec, outDataVec)
+      () => new conv2DSerialMainDirect()(testParams)) {
+      c => new conv2DSerialTest01(c)(inAddrVec, inDataVec, outAddrVec, outDataVec, coeffsAddr, coeffsData,
+        inAddrBegin, outAddrBegin, coeffsAddrBegin )
     } should be(true)
   }
 }
