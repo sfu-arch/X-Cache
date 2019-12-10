f77603f63e2d5d4ac036a4376e601c475c11b011
Amirali Sharifian <am.sharifian@gmail.com>
2019-12-10 12:59:16 -0800

    Update test cases and readme

--- python/test14.py
+++ python/test14.py
@@ -1,4 +1,5 @@
 import numpy as np
+import platform
 import dsim
 
 
@@ -8,7 +9,11 @@ b = np.array([0, 0, 0, 0, 0])
 a_s = dsim.DArray(a)
 b_s = dsim.DArray(b)
 
-hw_lib_path = "./hardware/chisel/build/libhw.so"
+if platform.system() == 'Linux':
+    hw_lib_path = "./hardware/chisel/build/libhw.so"
+elif platform.system() == 'Darwin':
+    hw_lib_path = "./hardware/chisel/build/libhw.dylib"
+
 
 cycle = dsim.sim(ptrs = [a_s, b_s], vars= [5], hwlib = hw_lib_path)
 
