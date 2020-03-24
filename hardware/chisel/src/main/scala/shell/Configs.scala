4e77903c86cf79c53ea205ccfc0b95fa43105287
Amirali Sharifian <am.sharifian@gmail.com>
2020-03-23 22:58:07 -0700

    Update cache reference

--- hardware/chisel/src/main/scala/shell/Configs.scala
+++ hardware/chisel/src/main/scala/shell/Configs.scala
@@ -5,18 +5,20 @@ import chisel3.util._
 import chipsalliance.rocketchip.config._
 import dandelion.config._
 import dandelion.interfaces.axi._
+import dandelion.junctions._
 
 
 /** SimDefaultConfig. Shell configuration for simulation */
 
 /**
-  * Please note that the dLen from WithSimShellConfig should be the same value as
-  * AXI -- memParams:dataBits
-  * @param vcrParams
-  * @param vmeParams
-  * @param hostParams
-  * @param memParams
-  */
+ * Please note that the dLen from WithSimShellConfig should be the same value as
+ * AXI -- memParams:dataBits
+ *
+ * @param vcrParams
+ * @param vmeParams
+ * @param hostParams
+ * @param memParams
+ */
 class WithShellConfig(vcrParams: DandelionVCRParams = DandelionVCRParams(),
                       vmeParams: DandelionVMEParams = DandelionVMEParams(),
                       hostParams: AXIParams = AXIParams(
@@ -24,13 +26,15 @@ class WithShellConfig(vcrParams: DandelionVCRParams = DandelionVCRParams(),
                       memParams: AXIParams = AXIParams(
                         addrBits = 32, dataBits = 64, userBits = 5,
                         lenBits = 4, // limit to 16 beats, instead of 256 beats in AXI4
-                        coherent = true))
+                        coherent = true),
+                      nastiParams: NastiParameters = NastiParameters(dataBits = 64, addrBits = 32, idBits = 13))
   extends Config((site, here, up) => {
     // Core
     case VCRKey => vcrParams
     case VMEKey => vmeParams
     case HostParamKey => hostParams
     case MemParamKey => memParams
+    case NastiKey => nastiParams
   }
   )
 
@@ -41,32 +45,35 @@ class WithSimShellConfig(dLen: Int = 64, pLog: Boolean = false)
 
 
 /**
-  * F1 Config
-  * AXI -- memParams:dataBits
-  * @param vcrParams
-  * @param vmeParams
-  * @param hostParams
-  * @param memParams
-  */
+ * F1 Config
+ * AXI -- memParams:dataBits
+ *
+ * @param vcrParams
+ * @param vmeParams
+ * @param hostParams
+ * @param memParams
+ */
 class WithF1Config(vcrParams: DandelionVCRParams = DandelionVCRParams(),
-                      vmeParams: DandelionVMEParams = DandelionVMEParams(),
-                      hostParams: AXIParams = AXIParams(
-                        addrBits = 32, dataBits = 32, idBits = 13, lenBits = 8),
-                      memParams: AXIParams = AXIParams(
-                        addrBits = 64, dataBits = 512, userBits = 10,
-                        lenBits = 8, // limit to 16 beats, instead of 256 beats in AXI4
-                        coherent = false))
+                   vmeParams: DandelionVMEParams = DandelionVMEParams(),
+                   hostParams: AXIParams = AXIParams(
+                     addrBits = 32, dataBits = 32, idBits = 13, lenBits = 8),
+                   memParams: AXIParams = AXIParams(
+                     addrBits = 64, dataBits = 512, userBits = 10,
+                     lenBits = 8, // limit to 16 beats, instead of 256 beats in AXI4
+                     coherent = false),
+                   nastiParams: NastiParameters = NastiParameters(dataBits = 64, addrBits = 32, idBits = 13))
   extends Config((site, here, up) => {
     // Core
     case VCRKey => vcrParams
     case VMEKey => vmeParams
     case HostParamKey => hostParams
     case MemParamKey => memParams
+    case NastiKey => nastiParams
   }
   )
 
 class WithF1ShellConfig(dLen: Int = 64, pLog: Boolean = false)
-                        (nPtrs: Int = 4, nVals: Int = 2, nRets: Int = 1, nCtrl: Int = 1, nEvent: Int = 1) extends Config(
+                       (nPtrs: Int = 4, nVals: Int = 2, nRets: Int = 1, nCtrl: Int = 1, nEvent: Int = 1) extends Config(
   new WithAccelConfig(DandelionAccelParams(dataLen = dLen, printLog = pLog)) ++
     new WithF1Config(DandelionVCRParams(numCtrl = nCtrl, numEvent = nEvent, numPtrs = nPtrs, numVals = nVals, numRets = nRets)))
 
