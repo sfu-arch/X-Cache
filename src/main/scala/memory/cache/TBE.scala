bdd764abcdb555cec8e8fa56aa3dc274444561c9
Ali-Sed <asedagha@sfu.ca>
2020-10-19 09:42:16 -0700

    methods added

--- src/main/scala/memory/cache/TBE.scala
+++ src/main/scala/memory/cache/TBE.scala
@@ -4,6 +4,9 @@ import chipsalliance.rocketchip.config._
 import chisel3._
 import dandelion.config._
 import dandelion.memory.cache.{HasCacheAccelParams, State}
+import chisel3.util._
+import chisel3.util.Enum
+
 
 class TBE (implicit p: Parameters)  extends AXIAccelBundle with HasCacheAccelParams {
   val state = new State()
@@ -22,8 +25,10 @@ class TBETableIO (implicit val p: Parameters) extends Bundle
   with HasCacheAccelParams
   with HasAccelShellParams {
 
-  val inputValue = Input(new TBE)
-  val output = Output(new TBE)
+  val addr = Input(UInt(xlen.W))
+  val command = Input(UInt())
+  val inputTBE= Input(new TBE)
+  val outputTBE = Output(new TBE)
 
 }
 
@@ -31,7 +36,54 @@ class   TBETable(Size: Int )(implicit  val p: Parameters) extends Module
   with HasCacheAccelParams
   with HasAccelShellParams {
 
+
+  val (idle::alloc :: dealloc :: read :: Nil) = Enum(4)
+
   val io = IO(new TBETableIO())
-  val TBEMemory = VecInit(seq.fill(Size) ((TBE.default)))
+  val TBEMemory = VecInit(Seq.fill(Size)(new TBE))
+  val TBEValid = VecInit(Seq.fill(Size)(RegInit(false.B)))
+  val TBEAddr = VecInit(Seq.fill(Size)(RegInit(0.U)))
+
+  val isAlloc = Wire(Bool())
+  val isDealloc = Wire(Bool())
+  val isRead = Wire(Bool())
+  val inc = Wire(Bool())
+
+
+  //val (idx,full) = Counter (inc, Size)
+  val idx = Wire(UInt(log2Ceil(Size).W))
+
+  when(isAlloc) {
+    for (i <- 0 until Size) {
+      when(TBEValid(i) === false.B) {
+        idx := i.asUInt()
+      }
+    }
+    TBEMemory(idx) := io.inputTBE
+    TBEAddr(idx) := io.addr
+    TBEValid(idx) := true.B
+    //    inc := true.B
+  }.elsewhen(isDealloc) {
+    for (i <- 0 until Size) {
+      when(TBEAddr(i) === io.addr & TBEValid(i) === true.B) {
+        idx := i.asUInt()
+      }
+    }
+    TBEValid(idx) := false.B
+  }.elsewhen(isRead) {
+    for (i <- 0 until Size) {
+      when(TBEAddr(i) === io.addr & TBEValid(i) === true.B) {
+        idx := i.asUInt()
+      }
+    }
+    io.outputTBE := TBEMemory(idx)
+
+  }
+
+
+  isAlloc := Mux( io.command === alloc, true.B, false.B)
+  isDealloc :=  Mux(io.command === dealloc, true.B, false.B)
+  isRead := Mux(io.command === read, true.B, false.B)
+
 
 }
