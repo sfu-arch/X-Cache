d993cbf5fa8e79f6b8909923a75b850dcfe64a9b
Ali-Sed <asedagha@sfu.ca>
2020-10-19 09:42:39 -0700

    minor changes

--- src/main/scala/memory/cache/AXICache.scala
+++ src/main/scala/memory/cache/AXICache.scala
@@ -23,7 +23,7 @@ class State(implicit p: Parameters) extends AXIAccelBundle with HasCacheAccelPar
 
 object MetaData  {
 
-  def apply(tag_len: Int): MetaData = {
+  def apply(tag_len: Int)(implicit p: Parameters): MetaData = {
     val wire = Wire(new MetaData)
     wire.tag := tag_len.U
     wire
