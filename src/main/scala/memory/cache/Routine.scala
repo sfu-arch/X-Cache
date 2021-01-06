060d2fe792cc6e2a694e6b35e5d62a612fd5cd22
Ali-Sed <asedagha@sfu.ca>
2021-01-06 00:11:58 -0800

    adding probe to test case

--- src/main/scala/memory/cache/Routine.scala
+++ src/main/scala/memory/cache/Routine.scala
@@ -11,7 +11,7 @@ object RoutineROM {
 
   val routineActions = Array [RoutinePC](
 
-    Routine ("LOAD_I") , Actions(Seq("Allocate", "ReadInt")),
+    Routine ("LOAD_I") , Actions(Seq("Probe","Allocate", "ReadInt")),
     Routine ("LOAD_M"), Actions(Seq ("DataRQ")),
     Routine("STORE_I"), Actions(Seq("Allocate")) // @todo incompleted
 
