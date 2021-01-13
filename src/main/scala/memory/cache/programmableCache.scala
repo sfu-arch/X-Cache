79687b475e01562937118cc335116dd7c2377823
Ali-Sed <asedagha@sfu.ca>
2021-01-13 12:13:49 -0800

    minor changes

--- src/main/scala/memory/cache/programmableCache.scala
+++ src/main/scala/memory/cache/programmableCache.scala
@@ -25,11 +25,15 @@ class programmableCache (implicit val p:Parameters) extends Module
 with HasCacheAccelParams
 with HasAccelShellParams{
 
+
     val io = IO(new programmableCacheIO())
 
     val cache = Module(new Gem5Cache())
     val tbe = Module(new TBETable())
     val state = Wire(UInt(stateLen.W))
+
+    cache.io.cpu <> DontCare
+    cache.io.mem <> DontCare
 
     val inputTBE = WireInit(TBE.default)
 
