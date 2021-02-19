f439d54805b2b9f577b72690314ddaf5e79fdc74
Ali-Sed <asedagha@sfu.ca>
2021-02-19 12:50:23 -0800

    dandelionTestDCRAccel updated for memGen

--- hardware/chisel/src/test/scala/accel/TestDCRAccel.scala
+++ hardware/chisel/src/test/scala/accel/TestDCRAccel.scala
@@ -4,7 +4,9 @@ import chisel3.MultiIOModule
 import chipsalliance.rocketchip.config._
 import dandelion.config._
 import dandelion.generator._
+import dandelion.shell._
 import dandelion.accel.{DandelionAccelDCRModule, DandelionAccelDebugModule, DandelionAccelModule}
+import memGen.shell._
 
 
 /**
@@ -13,7 +15,7 @@ import dandelion.accel.{DandelionAccelDCRModule, DandelionAccelDebugModule, Dand
  * @param p
  * @tparam T
  */
-abstract class DandelionTestDCRAccel[T <: DandelionAccelDCRModule](implicit val p: Parameters) extends MultiIOModule with HasAccelShellParams
+abstract class DandelionTestDCRAccel[T <: memGenModule](implicit val p: Parameters) extends MultiIOModule with HasAccelShellParams
 
 object DandelionTestDCRAccel {
 
@@ -24,14 +26,9 @@ object DandelionTestDCRAccel {
    * @param p
    * @return
    */
-  def apply(testName: String)(implicit p: Parameters): DandelionAccelDCRModule = {
+  def apply(testName: String)(implicit p: Parameters): memGenModule = {
     testName match {
-      case "test04" => new test04DF()
-      case "test05" => new test05DF()
-      case "test06" => new test06DF()
-      case "test07" => new test07DF()
-      case "test08" => new test08DF()
-      case "test09" => new test09DF()
+      case "memGenAccel" => new memGenAccel()
       case _ => throw new Exception(s"[EXCEPTION] The accel's name is not defined -- " +
         s"Please check the accel name you have passed: ${testName}")
     }
