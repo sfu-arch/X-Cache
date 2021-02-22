65a186aa97d55d4abb298859a579e4a60582675a
Ali-Sed <asedagha@sfu.ca>
2021-02-21 23:40:19 -0800

    adding object to message

--- src/main/scala/message/messageType.scala
+++ src/main/scala/message/messageType.scala
@@ -24,11 +24,35 @@ MessageParams{
     val data = UInt (dataLen.W)
 }
 
+object IntraNodeBundle {
+    def default(implicit p:Parameters) : IntraNodeBundle = {
+        val node = Wire(new IntraNodeBundle()(p))
+        node.addr := 0.U
+        node.inst := 0.U
+        node.data := 0.U
+        node
+    }
+
+    def default(node: IntraNodeBundle)(implicit p:Parameters) : IntraNodeBundle = {
+        node.addr := 0.U
+        node.inst := 0.U
+        node.data := 0.U
+        node
+    }}
+
 class MessageBundle (implicit p :Parameters) extends IntraNodeBundle()(p){
     val src = UInt(srcLen.W)
     val dst = UInt(dstLen.W)
 }
-
+object MessageBundle {
+    def default(implicit p: Parameters): MessageBundle = {
+        val message = Wire(new MessageBundle()(p))
+        IntraNodeBundle.default(message)
+        message.src := 0.U
+        message.dst := 0.U
+        message
+    }
+}
 
 
 
