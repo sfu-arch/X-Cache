4f4c6387a7820d666bb0052f1365d41a36b072a2
Ali-Sed <asedagha@sfu.ca>
2021-02-21 23:40:32 -0800

    adding depacketizer

--- src/main/scala/message/packetizer.scala
+++ src/main/scala/message/packetizer.scala
@@ -10,14 +10,21 @@ import memGen.interfaces.axi._
 import chisel3.util.experimental.loadMemoryFromFile
 
 
-class PacketizerIO [T<: IntraNodeBundle](MB : T) (implicit val p: Parameters) extends Bundle with MessageParams {
+class PacketizerIO [T<: IntraNodeBundle](MB : T = IntraNodeBundle) (implicit val p: Parameters) extends Bundle with MessageParams {
 
     val input = Flipped(Valid(MB.cloneType))
     val output = Valid(MB.cloneType)
 }
 
 
-class Packetizer [T <: IntraNodeBundle](MB : T) (implicit val p: Parameters) extends Module with MessageParams{
+class Packetizer [T <: IntraNodeBundle](MB : T = IntraNodeBundle ) (implicit val p: Parameters) extends Module with MessageParams{
+    val io = IO(new PacketizerIO(MB))
+
+    io.output.bits <> io.input.bits
+
+}
+
+class Depacketizer [T <: IntraNodeBundle](MB : T) (implicit val p: Parameters) extends Module with MessageParams{
     val io = IO(new PacketizerIO(MB))
 
     io.output.bits <> io.input.bits
