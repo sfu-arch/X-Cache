780dd342d056f6e594cc26eb8aff7f4333cfefdf
Ali-Sed <asedagha@sfu.ca>
2021-02-24 13:40:17 -0800

    updating routines

--- src/main/scala/memory/cache/Routine.scala
+++ src/main/scala/memory/cache/Routine.scala
@@ -8,11 +8,12 @@ object RoutineROM {
     // @todo should be fixed
     Routine ("LOAD_I") , Actions(Seq( "AllocateTBE","Allocate", "DataRQ", "SetState")),DstState("ID"),
     Routine ("LOAD_M"), Actions(Seq ("DataRQ", "SetState")),  DstState("M"),
-    Routine("STORE_I"), Actions(Seq("AllocateTBE","Allocate", "DataRQ", "SetState")),DstState("IM"),
+    Routine("STORE_I"), Actions(Seq("AllocateTBE","Allocate", "WrInt", "SetState")),DstState("M"),
     Routine ("LOAD_ID") , Actions(Seq( "ReadInt", "DeallocateTBE", "SetState")), DstState("D"),
     Routine ("LOAD_IM") , Actions(Seq( "ReadInt","DeallocateTBE", "SetState")), DstState("M"),
     Routine ("STORE_IS") , Actions(Seq( "WrInt", "SetState")), DstState("S"),
-    Routine("STORE_IM"), Actions(Seq("Allocate", "DataRQ", "SetState")),DstState("M")
+    Routine ("DATA_ID") , Actions(Seq( "WrInt","DeallocateTBE", "SetState")), DstState("D")
+
 
 
 
