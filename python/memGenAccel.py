367b5e248d02c9937ff4e3ab7e9667813e7fd647
Ali-Sed <asedagha@sfu.ca>
2021-02-26 18:00:45 -0800

    fixing the base address problem

--- python/memGenAccel.py
+++ python/memGenAccel.py
@@ -3,13 +3,14 @@ import platform
 import dsim
 
 
-
+a = np.array([i for i in range (1000)], dtype = np.uint32)
 if platform.system() == 'Linux':
     hw_lib_path = "./hardware/chisel/build/libhw.so"
 elif platform.system() == 'Darwin':
     hw_lib_path = "./hardware/chisel/build/libhw.dylib"
 
-events = dsim.sim(ptrs = [  ], vars= [0, 4, 2, 0,32,2], debugs=[], numRets=0, numEvents=0, hwlib = hw_lib_path)
+a = dsim.DArray(a ,  dsim.DArray.DType.UInt32)
+events = dsim.sim(ptrs = [a ], vars= [0, 4, 2, 0,32,2], debugs=[], numRets=0, numEvents=1, hwlib = hw_lib_path)
 
 #print("Cycle: " + str(events[0]))
 
