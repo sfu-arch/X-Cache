212b58c0cb24ff187bcee428c9a6bcd2b8b6faec
Ali-Sed <asedagha@sfu.ca>
2021-02-28 23:48:47 -0800

    Bug beacuse of not having DATA_ID got fixed

--- src/main/scala/memory/cache/nextRoutine.scala
+++ src/main/scala/memory/cache/nextRoutine.scala
@@ -5,12 +5,14 @@ object nextRoutine {
 
     val routineTriggerList = Array [RoutinePC](
 
-                    Routine ("LOAD_I") ,Trigger( Seq ("LOAD" , "I")),
+                    Routine ("LOAD_I") ,DstState("ID"), Trigger( Seq ("LOAD" , "I")),
                     Routine ("LOAD_M") ,  DstState("I"), Trigger( Seq("LOAD", "M")),
-                    Routine ("STORE_I") , Trigger( Seq("STORE", "I")),
-                    Routine ("LOAD_ID") , DstState("M"),Trigger( Seq ("LOAD" , "ID")),
+                    Routine ("STORE_I") , DstState("M"), Trigger( Seq("STORE", "I")),
+                    Routine ("LOAD_ID") , DstState("ID"),Trigger( Seq ("LOAD" , "ID")),
                     Routine ("LOAD_IM") ,  DstState("I"), Trigger( Seq("LOAD", "IM")),
-                    Routine ("STORE_IS") ,  DstState("IM"), Trigger( Seq("STORE", "IS"))
+                    Routine ("STORE_IS") ,  DstState("IM"), Trigger( Seq("STORE", "IS")),
+                    Routine ("DATA_ID"), DstState("D"), Trigger(Seq("DATA", "ID"))
+
 
 
 
