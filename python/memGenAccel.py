92f9ffdbc4551f23ca1054f6447387646a2d671c
Ali-Sed <asedagha@sfu.ca>
2021-02-28 23:45:47 -0800

    cleaning python file

--- python/memGenAccel.py
+++ python/memGenAccel.py
@@ -3,7 +3,7 @@ import platform
 import dsim
 
 
-a = np.array([i for i in range (1000)], dtype = np.uint32)
+a = np.array([i for i in range (100000)], dtype = np.uint32)
 if platform.system() == 'Linux':
     hw_lib_path = "./hardware/chisel/build/libhw.so"
 elif platform.system() == 'Darwin':
@@ -12,7 +12,7 @@ elif platform.system() == 'Darwin':
 a = dsim.DArray(a ,  dsim.DArray.DType.UInt32)
                             #        inst|addr|data
 events = dsim.sim(ptrs = [a ], vars= [0, 4, 2,
-                                      0,32,2], debugs=[], numRets=0, numEvents=1, hwlib = hw_lib_path)
+                                      0,128,2], debugs=[], numRets=0, numEvents=1, hwlib = hw_lib_path)
 
 #print("Cycle: " + str(events[0]))
 
