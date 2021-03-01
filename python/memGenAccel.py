aa2647306652add499a5b83e1662e9b1dea73372
Ali-Sed <asedagha@sfu.ca>
2021-02-28 16:39:58 -0800

    python file updated

--- python/memGenAccel.py
+++ python/memGenAccel.py
@@ -10,7 +10,9 @@ elif platform.system() == 'Darwin':
     hw_lib_path = "./hardware/chisel/build/libhw.dylib"
 
 a = dsim.DArray(a ,  dsim.DArray.DType.UInt32)
-events = dsim.sim(ptrs = [a ], vars= [0, 4, 2, 0,32,2], debugs=[], numRets=0, numEvents=1, hwlib = hw_lib_path)
+                            #        inst|addr|data
+events = dsim.sim(ptrs = [a ], vars= [0, 4, 2,
+                                      0,32,2], debugs=[], numRets=0, numEvents=1, hwlib = hw_lib_path)
 
 #print("Cycle: " + str(events[0]))
 
