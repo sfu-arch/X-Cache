3a011662ae7bc0c62190f5c6a015dc61670120d8
Ali-Sed <asedagha@sfu.ca>
2021-03-02 19:43:06 -0800

    increasing load vec ops to 10

--- python/memGenAccel.py
+++ python/memGenAccel.py
@@ -3,16 +3,28 @@ import platform
 import dsim
 
 
-a = np.array([i for i in range (100000)], dtype = np.uint32)
+mainMem = np.array([i for i in range (100000)], dtype = np.uint64)
+localMem =  np.array([i for i in range (100000)], dtype = np.uint64)
 if platform.system() == 'Linux':
     hw_lib_path = "./hardware/chisel/build/libhw.so"
 elif platform.system() == 'Darwin':
     hw_lib_path = "./hardware/chisel/build/libhw.dylib"
 
-a = dsim.DArray(a ,  dsim.DArray.DType.UInt32)
+mainMem = dsim.DArray(mainMem ,  dsim.DArray.DType.UInt32)
+
+nVals = 10 
+input_inst = np.zeros(nVals, dtype=np.uint64)
+input_addr = np.array([i*4 for i in range (nVals)], dtype = np.uint64)
+input_data = np.zeros(nVals, dtype=np.uint64)
+
+vals = [list(inst) for inst in zip(input_inst, input_addr, input_data)]
+vals = [item  for sublist in vals for item in sublist  ]
+print(vals)
                             #        inst|addr|data
-events = dsim.sim(ptrs = [a ], vars= [0, 4, 2,
-                                      0,128,2], debugs=[], numRets=0, numEvents=1, hwlib = hw_lib_path)
+#events = dsim.sim(ptrs = [mainMem ], vars= [0, 4, 2,
+ #                                    0,128,2], debugs=[], numRets=0, numEvents=1, hwlib = hw_lib_path)
+
+events = dsim.sim(ptrs = [mainMem ], vars= vals, debugs=[], numRets=0, numEvents=1, hwlib = hw_lib_path)
 
 #print("Cycle: " + str(events[0]))
 
