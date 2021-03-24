459e08a2bf4863ecaac54dd7a5aac1b1d72ee49c
Ali-Sed <asedagha@sfu.ca>
2021-03-24 12:42:10 -0700

    connection to router & ring connection between routers

--- src/main/scala/memory/cache/memGenTopLevel.scala
+++ src/main/scala/memory/cache/memGenTopLevel.scala
@@ -7,11 +7,11 @@ import memGen.config._
 import memGen.interfaces._
 import memGen.interfaces.axi._
 import memGen.noc._
-import memGen.memory.message.IntraNodeBundle
+import memGen.memory.message._
 
 
 
-class memGenTopLevelIO(implicit val p:Parameters) extends Bundle
+class memGenTopLevelIO( implicit val p:Parameters) extends Bundle
 with HasCacheAccelParams
 with HasAccelShellParams {
 
@@ -20,57 +20,43 @@ with HasAccelShellParams {
     val mem = new AXIMaster(memParams)
 }
 
-class memGenTopLevel(implicit val p:Parameters) extends Module
+class memGenTopLevel(val numCache:Int =1, val numMemCtrl:Int = 1, implicit val p:Parameters) extends Module
 with HasCacheAccelParams
 with HasAccelShellParams {
 
     val io = IO(new memGenTopLevelIO())
 
-    // io.mem <> DontCare
-    val cacheNode = Module(new CacheNode())
-    val memCtrl = Module(new memoryWrapper())
-//    RegNext(io.instruction.bits) <> dut.io.instruction.bits
-//    dut.io.instruction.valid := RegNext(io.instruction.valid)
-//    io.instruction.ready := dut.io.instruction.ready
+    val cacheNode = for (i <- 0 until numCache) yield {
+        val Cache = Module (new CacheNode(UniqueID = i))
+        Cache
+    }
+    val routerNode = for (i <- 0 until numCache + numMemCtrl) yield {
+        val Router = Module(new Router(ID = i))
+        Router
+    }
+    val memCtrl = Module(new memoryWrapper(ID = (numCache + numMemCtrl - 1))(p))
+    val memCtrlInputQueue = Module(new Queue(new Flit(), entries = 16))
 
-    (io.instruction.bits) <> cacheNode.io.in.cpu.bits
-    cacheNode.io.in.cpu.valid:= (io.instruction.valid)
-    io.instruction.ready := cacheNode.io.in.cpu.ready
+    for (i <- 0 until numCache) {
+        (io.instruction.bits) <> cacheNode(i).io.in.cpu.bits
+        cacheNode(i).io.in.cpu.valid := (io.instruction.valid)
+        io.instruction.ready := cacheNode(i).io.in.cpu.ready
+        io.resp <>  cacheNode(i).io.out.cpu
+    }
 
-    cacheNode.io.in.otherNodes := DontCare
+    for (i <- 0 until numCache){
+        routerNode(i).io.cacheIn <> cacheNode(i).io.out.network
+        cacheNode(i).io.in.network <> routerNode(i).io.cacheOut
+    }
 
+    memCtrlInputQueue.io.enq <> routerNode(numCache + numMemCtrl).io.cacheOut
+    memCtrl.io.in <> memCtrlInputQueue.io.deq
+    routerNode(numCache + numMemCtrl).io.cacheIn <> memCtrl.io.out
     io.mem <> memCtrl.io.mem
 
-        val memCtrlInputQueue = Module(new Queue(new IntraNodeBundle(), entries = 5))
-        memCtrlInputQueue.io.enq.bits.data <> cacheNode.io.out.network.bits.data
-        memCtrlInputQueue.io.enq.bits.addr <> cacheNode.io.out.network.bits.addr
-        memCtrlInputQueue.io.enq.bits.inst <> cacheNode.io.out.network.bits.inst
-        memCtrlInputQueue.io.enq.valid := cacheNode.io.out.network.valid
-
-        io.resp.bits.addr := cacheNode.io.out.cpu.bits.addr
-        io.resp.bits.data := cacheNode.io.out.cpu.bits.data
-        io.resp.bits.inst := cacheNode.io.out.cpu.bits.inst
-        io.resp.valid := cacheNode.io.out.cpu.valid
-        cacheNode.io.out.cpu.ready := io.resp.ready
-
-        memCtrl.io.in.bits <> memCtrlInputQueue.io.deq.bits
-        memCtrl.io.in.valid := memCtrlInputQueue.io.deq.valid
-        memCtrlInputQueue.io.deq.ready :=  memCtrl.io.in.ready
-//    memCtrl.io.in <> cacheNode.io.out
-    // printf(p" valid :${memCtrl.io.in.valid} , addr: ${memCtrl.io.in.bits.addr} \n")
-
-//    memCtrl
-      cacheNode.io.in.memCtrl.bits.inst  := memCtrl.io.out.bits.inst
-      cacheNode.io.in.memCtrl.bits.addr := memCtrl.io.out.bits.addr
-      cacheNode.io.in.memCtrl.bits.data := memCtrl.io.out.bits.data
-      cacheNode.io.in.memCtrl.valid     :=  memCtrl.io.out.valid
-
-
-//        cacheNode.io.in.memCtrl <> DontCare
-
-//    memCtrl.io.in := cacheNode.
-
-
-
+    for (i <- 0 until numCache + numMemCtrl - 1){
+         routerNode(i+1).io.in <> routerNode(i).io.out
+    }
+    routerNode(0).io.in <> routerNode(numCache + numMemCtrl).io.out
 
 }
