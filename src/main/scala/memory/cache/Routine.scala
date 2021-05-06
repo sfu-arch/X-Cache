b1e9571134300f6c405a8c2086d9c3e6a9c8b8a4
Ali-Sed <asedagha@sfu.ca>
2021-05-06 10:40:03 -0700

    minor changes

--- src/main/scala/memory/cache/Routine.scala
+++ src/main/scala/memory/cache/Routine.scala
@@ -13,12 +13,26 @@ object RoutineROM {
     //10-11
     Routine ("LOAD_ID") , Actions(Seq( "SetState")), DstState("ID"),
     Routine ("LOAD_E") , Actions(Seq( "SetState")), DstState("E"),
-    Routine ("DATA_ID") , Actions(Seq( "WrInt","ReadInt","DeallocateTBE","SetState")), DstState("E"),
+    Routine ("DATA_ID") , Actions(Seq( "WrInt","RdInt","DeallocateTBE","SetState")), DstState("E"),
     Routine ("STORE_E") , Actions(Seq( "WrInt","SetState")), DstState("E"),
     Routine ("NOP_E") , Actions(Seq("SetState")), DstState("E"),
     Routine ("NOP_I") , Actions(Seq("SetState")), DstState("I"),
     Routine ("NOP_ID") , Actions(Seq("SetState")), DstState("ID"),
     Routine ("STORE_IS") , Actions(Seq( "WrInt", "SetState")), DstState("S"),
+
+
+    // Prefetch_I : Allocate, AllocateTBE, DataRQ 16, Feedback 1, SetState("PF_It")
+    // Prefetch_It: Allocate, AllocateTBE, BRZ 1, Feedback 1, SetState("It") 
+    // Data_PF_IT :
+
+    // node: 0
+    // WideLoad_I: AllocateTBE, BRNF 3, Allocate, DataRQ 2, SetState("WL"), SetState("WL_FWD")
+    // DATA_WL: Write, DeallocateTBE
+    // DATA_WL-FWD: PUSHRQ TOOthers Node_1, Deallocate TBE
+    
+
+    // node: 1
+    // WideLoad_I : AllocateTBE, Allocate, SetState("WL")
 
 
     
