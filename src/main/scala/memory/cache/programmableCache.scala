3367402ce8ff23ebaaa826de5a586f3349ede7fd
Ali-Sed <asedagha@sfu.ca>
2022-05-06 13:48:14 -0700

    Revert "************** Temporary change in signal of design ************"
    This reverts commit d0aace4d3b3ca7b279b29e429ae39ea0ec9a0916.

--- src/main/scala/memory/cache/programmableCache.scala
+++ src/main/scala/memory/cache/programmableCache.scala
@@ -390,12 +390,8 @@ with HasAccelShellParams{
         actionReg(i).io.enq.bits.tbeFields := pcWire(i).tbeFields
 
         firstLineNextRoutine(i) := actionRom(i)(pcWire(i).pc).asUInt() === ActionList.actions("NOP").asUInt()// NOP
-        // *************** IMPORTANT: + has been changed to -, just for the sake of the syn benchmark *********
-//        updatedPC(i) := Mux(firstLineNextRoutine(i), pcWire(i).pc, pcWire(i).pc + 1.U + compUnit(i).io.pc )
-        updatedPC(i) := Mux(firstLineNextRoutine(i), pcWire(i).pc, pcWire(i).pc + 1.U - compUnit(i).io.pc )
-
+        updatedPC(i) := Mux(firstLineNextRoutine(i), pcWire(i).pc, pcWire(i).pc + 1.U + compUnit(i).io.pc )
         updatedPCValid(i) := !firstLineNextRoutine(i)
-
 
         updateWay(i) := pc.io.read(i).out.bits.way === nWays.U & cache.io.cpu(i).resp.fire()
         pc.io.read(i).in.bits.data.addr := DontCare //
